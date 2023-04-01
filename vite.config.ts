906c9b4c216305b37c6c77671d20606918cfae2f
Jonas Jongejan <jonas@halfdanj.dk>
2023-04-01 13:06:13 -0400

    🧪 Add component vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		// Jest like globals
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		setupFiles: ['./scripts/setup-test.js']
 	}
 });
