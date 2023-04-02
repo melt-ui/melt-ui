5c047e2f0f956296ae0baee782bd22ae3f3fdf33
Jonas Jongejan <jonas@halfdanj.dk>
2023-04-01 20:26:50 -0400

    Renaming *.pw.ts to *.test.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
+		include: ['src/**/*.{spec}.{js,ts}'],
 	}
 });
