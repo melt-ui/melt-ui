cc0142174bb82cfaff47a0da9080bf1d385814b3
Jonas Jongejan <jonas@halfdanj.dk>
2023-04-03 17:57:12 -0400

    Export thumb props

--- src/lib/components/Switch/index.ts
+++ src/lib/components/Switch/index.ts
@@ -2,6 +2,7 @@ import Root from './root.svelte';
 import Thumb from './thumb.svelte';
 
 export type { SwitchRootProps } from './root.svelte';
+export type { SwitchThumbProps } from './thumb.svelte';
 export const Switch = {
 	Root,
 	Thumb
