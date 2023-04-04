1cd04082ba7b813515cc4d0b490ff87290048976
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-04 22:43:14 +0100

    :art: adapt switch cmp to use reactiveContext

--- src/lib/components/Switch/thumb.svelte
+++ src/lib/components/Switch/thumb.svelte
@@ -1,13 +1,17 @@
 <script lang="ts" context="module">
 	import type { BaseProps } from '$lib/types';
-	import { getSwitchContext, getState } from './root.svelte';
+	import { getRootContext, getState } from './root.svelte';
 
 	export type SwitchThumbProps = BaseProps<HTMLSpanElement>;
 </script>
 
 <script lang="ts">
 	type $$Props = SwitchThumbProps;
-	const { checked, disabled } = getSwitchContext();
+	const rootCtx = getRootContext();
 </script>
 
-<span {...$$restProps} data-state={getState($checked)} data-disabled={$disabled ? '' : undefined} />
+<span
+	{...$$restProps}
+	data-state={getState($rootCtx.checked)}
+	data-disabled={$rootCtx.disabled ? '' : undefined}
+/>
