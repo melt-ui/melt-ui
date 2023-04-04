cd72da8b0abffc5be806d4bbaeba44006e0ebf97
Jonas Jongejan <jonas@halfdanj.dk>
2023-04-04 09:48:08 -0400

    add getPropsObj to frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+	import { getPropsObj } from './(previews)/helpers';
 	import schemas from './(previews)/schemas';
+
+	function getPropsObjForSchema(schema: (typeof schemas)[keyof typeof schemas]): any {
+		return getPropsObj<{}>(schema.meta);
+	}
 </script>
 
 <div class="wrapper">
 	{#each Object.entries(schemas) as [title, schema]}
+		{@const propsObj = getPropsObjForSchema(schema)}
 		<div class="flex h-full flex-col gap-2 overflow-hidden">
 			<a href={`/${title}`} class="flex items-baseline justify-between">
 				<h2 class="text-2xl font-bold capitalize text-white">{title}</h2>
 				<span class="text-sm text-slate-300">View docs</span></a
 			>
 			<div class="comp-preview grow place-items-center">
-				<svelte:component this={schema.example} />
+				<svelte:component this={schema.example} {propsObj} />
 			</div>
 		</div>
 	{/each}
