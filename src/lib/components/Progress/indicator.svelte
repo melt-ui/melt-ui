a3996517cd36867fdb5ad7c31a9205cb47073382
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-04-06 17:38:26 +0200

    feat: progress

---
+++ src/lib/components/Progress/indicator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	import type { BaseProps } from '$lib/types';
+	import { getRootContext, getState } from './root.svelte';
+
+	export type Props = BaseProps<HTMLDivElement>;
+</script>
+
+<script lang="ts">
+	type $$Props = Props;
+
+	const ctx = getRootContext();
+</script>
+
+<div
+	{...$$restProps}
+	data-value={$ctx.value}
+	data-max={$ctx.max}
+	data-state={getState($ctx.value, $ctx.max)}
+>
+	<slot />
+</div>
---
+++ src/lib/components/Progress/root.svelte
@@ -0,0 +1,57 @@
+<script lang="ts" context="module">
+	import { reactiveContext } from '$lib/helpers/reactiveContext';
+	import type { BaseProps } from '$lib/types';
+
+	export type ProgressRootProps = BaseProps<HTMLProgressElement> & {
+		/** The maximum progress value. */
+		max?: number;
+		/** The progress value. */
+		value?: number | null;
+
+		readonly 'data-state'?: 'complete' | 'indeterminate' | 'loading';
+		readonly 'data-value'?: ProgressRootProps['value'];
+		readonly 'data-max'?: ProgressRootProps['max'];
+	};
+
+	type ProgressContext = {
+		max?: number;
+		value?: number | null;
+	};
+
+	const { getContext, setContext } = reactiveContext<ProgressContext>();
+	export const getRootContext = getContext;
+
+	export function getState(
+		value: ProgressRootProps['value'],
+		max: ProgressRootProps['max']
+	): ProgressRootProps['data-state'] {
+		return value == null ? 'indeterminate' : value >= (max ?? 1) ? 'complete' : 'loading';
+	}
+</script>
+
+<script lang="ts">
+	type $$Props = ProgressRootProps;
+
+	export let max: $$Props['max'] = 1;
+	export let value: $$Props['value'] = null;
+
+	const ctxStore = setContext({
+		max: [max, (v) => (max = v)],
+		value: [value, (v) => (value = v)]
+	});
+	$: ctxStore.set({ value, max });
+</script>
+
+<div
+	role="progressbar"
+	data-value={value}
+	data-max={max}
+	data-state={getState(value, max)}
+	aria-valuemin={0}
+	aria-valuemax={max}
+	aria-valuenow={value}
+	aria-valuetext={`${(100 * (value ?? 0)) / (max ?? 1)}%`}
+	{...$$restProps}
+>
+	<slot />
+</div>
