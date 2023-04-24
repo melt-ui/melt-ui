9350d7c4fb9b460a3273c5791a966b137da144c6
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-24 23:15:27 +0100

    feat: add github & discord links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,9 @@
 	import '@fontsource/inter/800.css';
 	import '@fontsource/inter/900.css';
 
+	import GithubLogo from '~icons/radix-icons/github-logo';
+	import DiscordLogo from '~icons/radix-icons/discord-logo';
+
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
@@ -16,13 +19,33 @@
 </script>
 
 <main class="flex min-h-screen flex-col">
-	<nav class="flex items-center justify-between border-b border-zinc-400 px-4 py-2">
-		<a class="flex items-center gap-2 text-xl font-sans font-bold" href="/">
-			<img class="h-9 w-9 rounded-sm object-contain" src="/logo.svg" alt="Radix and Svelte logos" />
+	<nav class="flex items-center justify-between gap-2 border-b border-zinc-400 px-2 lg:px-4 py-2">
+		<a class="flex items-center gap-2 lg:text-xl font-sans font-bold" href="/">
+			<img
+				class="w-7 h-7 lg:h-9 lg:w-9 rounded-sm object-contain"
+				src="/logo.svg"
+				alt="Radix and Svelte logos"
+			/>
 			Radix Svelte
 		</a>
 
-		<a href="/docs/accordion" class="underline hover:opacity-75">Documentation</a>
+		<div class="flex items-center gap-2 lg:gap-4">
+			<a
+				href="https://github.com/tglide/radix-svelte"
+				target="_blank"
+				class="hover:opacity-75 lg:text-xl"
+			>
+				<GithubLogo />
+			</a>
+			<a
+				href="https://discord.com/invite/gQrpPs34xH"
+				target="_blank"
+				class="hover:opacity-75 lg:text-xl"
+			>
+				<DiscordLogo />
+			</a>
+			<a href="/docs/accordion" class="underline hover:opacity-75">Documentation</a>
+		</div>
 	</nav>
 	<div class="flex grow flex-col">
 		<slot />
