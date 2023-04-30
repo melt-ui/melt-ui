9b3c830b98a422b98c3b13891ba62c98cf6e391e
tglide <26071571+TGlide@users.noreply.github.com>
2023-05-01 00:14:47 +0100

    fix: radio grouup accesibility tests

--- src/lib/components/RadioGroup/tests/RadioGroupTest.svelte
+++ src/lib/components/RadioGroup/tests/RadioGroupTest.svelte
@@ -3,18 +3,13 @@
 </script>
 
 <RadioGroup.Root aria-label="View density">
-	<RadioGroup.Item value="default" id="r1">
+	<RadioGroup.Item value="default" id="r1" aria-labelledby="r1-label">
 		<RadioGroup.Indicator />
 	</RadioGroup.Item>
-	<label for="r1"> Default </label>
+	<label for="r1" id="r1-label"> Default </label>
 
-	<RadioGroup.Item value="comfortable" id="r2">
+	<RadioGroup.Item value="comfortable" id="r2" aria-labelledby="r2-label">
 		<RadioGroup.Indicator />
 	</RadioGroup.Item>
-	<label for="r2"> Comfortable </label>
-
-	<RadioGroup.Item value="compact" id="r3">
-		<RadioGroup.Indicator />
-	</RadioGroup.Item>
-	<label for="r3"> Compact </label>
+	<label for="r2" id="r2-label"> Comfortable </label>
 </RadioGroup.Root>
--- src/routes/(previews)/RadioGroup/example.svelte
+++ src/routes/(previews)/RadioGroup/example.svelte
@@ -22,10 +22,11 @@
 		hover:bg-vermilion-100 focus:ring-2 focus:ring-black"
 			value="default"
 			id="r1"
+			aria-labelledby="r1-label"
 		>
 			<RadioGroup.Indicator class="h-3 w-3 rounded-full bg-vermilion-500 " />
 		</RadioGroup.Item>
-		<label class="leading-none text-white" for="r1"> Default </label>
+		<label class="leading-none text-white" for="r1" id="r1-label"> Default </label>
 	</div>
 	<div class="flex items-center gap-3">
 		<RadioGroup.Item
@@ -33,10 +34,11 @@
 		hover:bg-vermilion-100 focus:ring-2 focus:ring-black"
 			value="comfortable"
 			id="r2"
+			aria-labelledby="r2-label"
 		>
 			<RadioGroup.Indicator class="h-3 w-3 rounded-full bg-vermilion-500 " />
 		</RadioGroup.Item>
-		<label class="leading-none text-white" for="r2"> Comfortable </label>
+		<label class="leading-none text-white" for="r2" id="r2-label"> Comfortable </label>
 	</div>
 	<div class="flex items-center gap-3">
 		<RadioGroup.Item
@@ -44,9 +46,10 @@
 		hover:bg-vermilion-100 focus:ring-2 focus:ring-black"
 			value="compact"
 			id="r3"
+			aria-labelledby="r3-label"
 		>
 			<RadioGroup.Indicator class="h-3 w-3 rounded-full bg-vermilion-500 " />
 		</RadioGroup.Item>
-		<label class="leading-none text-white" for="r3"> Compact </label>
+		<label class="leading-none text-white" for="r3" id="r3-label"> Compact </label>
 	</div>
 </RadioGroup.Root>
