2e9afd922b0f6eddf1b11261cfc5f511bff6732b
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-30 13:19:29 +0100

    fix: lint fixes

--- src/lib/components/RadioGroup/tests/RadioGroupTest.svelte
+++ src/lib/components/RadioGroup/tests/RadioGroupTest.svelte
@@ -2,4 +2,19 @@
 	import { RadioGroup } from '../index';
 </script>
 
-<!-- TODO: Add test component code -->
+<RadioGroup.Root aria-label="View density">
+	<RadioGroup.Item value="default" id="r1">
+		<RadioGroup.Indicator />
+	</RadioGroup.Item>
+	<label for="r1"> Default </label>
+
+	<RadioGroup.Item value="comfortable" id="r2">
+		<RadioGroup.Indicator />
+	</RadioGroup.Item>
+	<label for="r2"> Comfortable </label>
+
+	<RadioGroup.Item value="compact" id="r3">
+		<RadioGroup.Indicator />
+	</RadioGroup.Item>
+	<label for="r3"> Compact </label>
+</RadioGroup.Root>
