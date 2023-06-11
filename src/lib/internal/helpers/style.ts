fdd9c5e6bd46eabe7658d3361c3b790b217acb2b
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-11 02:56:50 -0400

    add comments to code

--- src/lib/internal/helpers/style.ts
+++ src/lib/internal/helpers/style.ts
@@ -1,4 +1,9 @@
-// style object to string
+/**
+ * A utility function that converts a style object to a string.
+ *
+ * @param style - The style object to convert
+ * @returns The style object as a string
+ */
 export function styleToString(style: Record<string, number | string | undefined>): string {
 	return Object.keys(style).reduce((str, key) => {
 		if (style[key] === undefined) return str;
