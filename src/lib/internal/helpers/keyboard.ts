8d15583a3cedc22610051fc5acf42656fc66b0d6
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-11 18:41:21 -0400

    add keyboard constant object

---
+++ src/lib/internal/helpers/keyboard.ts
@@ -0,0 +1,23 @@
+/**
+ * A constant object that maps commonly used keyboard keys to their corresponding string values.
+ * This object can be used in other parts of the application to handle keyboard input and prevent
+ * hardcoding key values.
+ */
+export const kbd = {
+	BACKSPACE: 'Backspace',
+	CONTROL: 'Control',
+	DELETE: 'Delete',
+	DOWN_ARROW: 'ArrowDown',
+	END: 'End',
+	ENTER: 'Enter',
+	ESCAPE: 'Escape',
+	HOME: 'Home',
+	LEFT_ARROW: 'ArrowLeft',
+	PAGE_DOWN: 'PageDown',
+	PAGE_UP: 'PageUp',
+	RIGHT_ARROW: 'ArrowRight',
+	SHIFT: 'Shift',
+	SPACE: ' ',
+	TAB: 'Tab',
+	UP_ARROW: 'ArrowUp',
+};
