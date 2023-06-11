a05205e25d1b615ef3638a7fefe9a04fdaca01b4
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-11 02:19:04 -0400

    add event comments

--- src/lib/internal/helpers/event.ts
+++ src/lib/internal/helpers/event.ts
@@ -1,6 +1,15 @@
 import type { Arrayable } from '$lib/internal/types';
 
-// Function that dispatches and handles a custom event
+/**
+ * Handles and dispatches a custom event with the given name and detail.
+ *
+ * @template E - The type of the custom event to handle and dispatch
+ * @template OriginalEvent - The type of the original event that triggered the custom event
+ * @param name - The name of the custom event to handle and dispatch
+ * @param handler - The function to call when the custom event is triggered
+ * @param detail - An object containing the detail of the custom event
+ * @param detail.originalEvent - The original event that triggered the custom event
+ */
 export function handleAndDispatchCustomEvent<E extends CustomEvent, OriginalEvent extends Event>(
 	name: string,
 	handler: ((event: E) => void) | undefined,
@@ -12,7 +21,13 @@ export function handleAndDispatchCustomEvent<E extends CustomEvent, OriginalEven
 
 	target.dispatchEvent(event);
 }
-
+/**
+ * A type alias for a general event listener function.
+ *
+ * @template E - The type of event to listen for
+ * @param evt - The event object
+ * @returns The return value of the event listener function
+ */
 export type GeneralEventListener<E = Event> = (evt: E) => unknown;
 
 /**
@@ -40,7 +55,12 @@ export function addEventListener<EventType = Event>(
 ): VoidFunction;
 
 /**
- * Adds an event listener to an element, and returns a function to remove it.
+ * Adds an event listener to the specified target element(s) for the given event(s), and returns a function to remove it.
+ * @param target The target element(s) to add the event listener to.
+ * @param event The event(s) to listen for.
+ * @param handler The function to be called when the event is triggered.
+ * @param options An optional object that specifies characteristics about the event listener.
+ * @returns A function that removes the event listener from the target element(s).
  */
 export function addEventListener(
 	target: Window | Document | EventTarget,
@@ -50,7 +70,10 @@ export function addEventListener(
 ) {
 	const events = Array.isArray(event) ? [...event] : [event];
 
+	// Add the event listener to each specified event for the target element(s).
 	events.forEach((_event) => target.addEventListener(_event, handler, options));
+
+	// Return a function that removes the event listener from the target element(s).
 	return () => {
 		events.forEach((_event) => target.removeEventListener(_event, handler, options));
 	};
