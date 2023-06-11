db90e0d05d017477f51004a8855d3821fd2dfc3f
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-11 19:08:23 -0400

    update kbd constant

--- src/lib/internal/helpers/keyboard.ts
+++ src/lib/internal/helpers/keyboard.ts
@@ -1,23 +1,23 @@
 /**
  * A constant object that maps commonly used keyboard keys to their corresponding string values.
  * This object can be used in other parts of the application to handle keyboard input and prevent
- * hardcoding key values.
+ * hard-coded strings throughout.
  */
 export const kbd = {
+	ARROW_DOWN: 'ArrowDown',
+	ARROW_LEFT: 'ArrowLeft',
+	ARROW_RIGHT: 'ArrowRight',
+	ARROW_UP: 'ArrowUp',
 	BACKSPACE: 'Backspace',
 	CONTROL: 'Control',
 	DELETE: 'Delete',
-	DOWN_ARROW: 'ArrowDown',
 	END: 'End',
 	ENTER: 'Enter',
 	ESCAPE: 'Escape',
 	HOME: 'Home',
-	LEFT_ARROW: 'ArrowLeft',
 	PAGE_DOWN: 'PageDown',
 	PAGE_UP: 'PageUp',
-	RIGHT_ARROW: 'ArrowRight',
 	SHIFT: 'Shift',
 	SPACE: ' ',
 	TAB: 'Tab',
-	UP_ARROW: 'ArrowUp',
 };
