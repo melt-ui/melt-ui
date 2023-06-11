11c36b81121078109a16cc17fbd7fbc6896beb0c
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-11 02:59:59 -0400

    consolidate `is` functions

--- src/lib/internal/helpers/is.ts
+++ src/lib/internal/helpers/is.ts
@@ -1,3 +1,7 @@
 export const isBrowser = typeof window !== 'undefined';
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const isFunction = (v: unknown): v is Function => typeof v === 'function';
+
+export function isHTMLElement(element: unknown): element is HTMLElement {
+	return element instanceof HTMLElement;
+}
