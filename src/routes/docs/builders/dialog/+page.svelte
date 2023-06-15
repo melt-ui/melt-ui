9167a4faf01947b030c364219d2973a25f51c377
Manuel Da Silva <manueldasilva98@gmail.com>
2023-06-15 17:37:06 -0400

    dialog-drawer-example

--- src/routes/docs/builders/dialog/+page.svelte
+++ src/routes/docs/builders/dialog/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Docs } from '$routes/(components)';
 	import code from './code.ignore-svelte?raw';
+	import code2 from './code2.ignore-svelte?raw';
 	import Preview from './preview.svelte';
+	import Preview2 from './preview2.svelte';
 </script>
 
 <Docs.H1>Dialog</Docs.H1>
@@ -13,5 +15,11 @@
 <Preview />
 
 <Docs.CodeBlock {code} />
+
+<div class="my-16" />
+
+<Preview2 />
+
+<Docs.CodeBlock code={code2} />
 
 <Docs.Construction />
