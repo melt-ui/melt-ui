dda3b6475dab08e17a131a6ee953dce42206e661
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-16 17:38:06 -0400

    menu item selector

--- src/lib/builders/dropdown-menu/index.ts
+++ src/lib/builders/dropdown-menu/index.ts
@@ -9,12 +9,11 @@ import {
 	isBrowser,
 	kbd,
 	omit,
+	sleep,
 	styleToString,
 	uuid,
 } from '$lib/internal/helpers';
-import { sleep } from '$lib/internal/helpers/sleep';
 import type { Defaults } from '$lib/internal/types';
-import { tick } from 'svelte';
 import { derived, writable, type Writable } from 'svelte/store';
 
 /**
@@ -133,10 +132,16 @@ export function createDropdownMenu(args?: CreateDropdownMenuArgs) {
 		}),
 	}));
 
-	const item = elementMultiDerived([selected], ([$selected], { attach }) => {
+	const item = elementMultiDerived([selected, menu], ([$selected, $menu], { attach }) => {
 		return () => {
 			attach('keydown', (e) => {
-				if (e.key === kbd.ENTER || e.key === kbd.SPACE) {
+				console.log(e);
+				if (e.shiftKey && e.key === kbd.TAB) {
+					e.preventDefault();
+					e.stopPropagation();
+					e.stopImmediatePropagation();
+				}
+				if (e.key === kbd.ENTER || e.key === kbd.SPACE || e.key === kbd.TAB) {
 					e.preventDefault();
 				}
 			});
@@ -153,14 +158,10 @@ export function createDropdownMenu(args?: CreateDropdownMenuArgs) {
 
 			return {
 				role: 'menuitem',
-				tabindex: 0,
+				'data-melt-menu': $menu.id,
+				tabindex: -1,
 			};
 		};
-	});
-
-	let typed: string[] = [];
-	const resetTyped = debounce(() => {
-		typed = [];
 	});
 
 	effect([open, menu, activeTrigger], ([$open, $menu, $activeTrigger]) => {
@@ -168,14 +169,12 @@ export function createDropdownMenu(args?: CreateDropdownMenuArgs) {
 
 		const menuEl = getElementByMeltId($menu['data-melt-id']);
 		if (menuEl && $open) {
-			// Focus on selected option or first option
-			const selectedOption = menuEl.querySelector('[data-selected]') as HTMLElement | undefined;
-			if (!selectedOption) {
-				const firstOption = menuEl.querySelector('[role="option"]') as HTMLElement | undefined;
-				sleep(1).then(() => firstOption?.focus());
-			} else {
-				sleep(1).then(() => selectedOption.focus());
-			}
+			// Selector to get menu items belonging to menu
+			const itemSelector = `[role="menuitem"][data-melt-menu="${$menu.id}"]`;
+
+			// Focus on first menu item
+			const firstOption = document.querySelector(itemSelector) as HTMLElement | undefined;
+			sleep(1).then(() => firstOption?.focus());
 
 			const keydownListener = (e: KeyboardEvent) => {
 				if (e.key === kbd.ESCAPE) {
@@ -211,21 +210,6 @@ export function createDropdownMenu(args?: CreateDropdownMenuArgs) {
 					lastOption.focus();
 					return;
 				}
-
-				// Typeahead
-				const isAlphaNumericOrSpace = /^[a-z0-9 ]$/i.test(e.key);
-				if (isAlphaNumericOrSpace) {
-					typed.push(e.key.toLowerCase());
-					const typedString = typed.join('');
-					const matchingOption = allOptions.find((el) =>
-						el.innerText.toLowerCase().startsWith(typedString)
-					);
-					if (matchingOption) {
-						matchingOption.focus();
-					}
-
-					resetTyped();
-				}
 			};
 
 			document.addEventListener('keydown', keydownListener);
@@ -238,57 +222,10 @@ export function createDropdownMenu(args?: CreateDropdownMenuArgs) {
 		}
 	});
 
-	effect([menu], ([$menu]) => {
-		tick().then(() => {
-			const menuEl = getElementByMeltId($menu['data-melt-id']);
-			if (!menuEl) return;
-
-			const selectedOption = menuEl.querySelector('[role=option][data-selected]') as
-				| HTMLElement
-				| undefined;
-			if (selectedOption) {
-				const data = selectedOption.getAttribute('data-value');
-				if (data) {
-					if (selectedOption.getAttribute('data-type') === 'number') {
-						selectedText.set(+data);
-					} else {
-						selectedText.set(data);
-					}
-				}
-			}
-		});
-	});
-
-	const isSelected = derived([selected], ([$selected]) => {
-		return (value: string | number) => {
-			return $selected === value;
-		};
-	});
-
-	const input = derived([selected, options], ([$selected, $options]) => {
-		return {
-			type: 'hidden',
-			name: $options.name,
-			value: $selected,
-			'aria-hidden': true,
-			hidden: true,
-			tabIndex: -1,
-			required: $options.required,
-			disabled: $options.disabled,
-			style: styleToString({
-				position: 'absolute',
-				opacity: 0,
-				'pointer-events': 'none',
-				margin: 0,
-				transform: 'translateX(-100%)',
-			}),
-		};
-	});
-
-	return { trigger, menu, open, item, selected, selectedText, arrow, isSelected, options, input };
+	return { trigger, menu, open, item, selected, selectedText, arrow, options };
 }
 
-const TRIGGER_OPEN_KEYS = [kbd.ENTER, kbd.SPACE, kbd.ARROW_RIGHT, kbd.TAB];
+const TRIGGER_OPEN_KEYS = [kbd.ENTER, kbd.SPACE, kbd.ARROW_RIGHT];
 
 export type CreateSubMenuArgs = CreateDropdownMenuArgs & {
 	parentOpen: Writable<boolean>;
@@ -319,7 +256,7 @@ export function createSubMenu(args?: CreateSubMenuArgs) {
 		trigger: uuid(),
 	};
 
-	const menu = elementDerived(
+	const subMenu = elementDerived(
 		[open, activeTrigger, options],
 		([$open, $activeTrigger, $options], { addAction }) => {
 			if ($open && $activeTrigger) {
@@ -346,7 +283,7 @@ export function createSubMenu(args?: CreateSubMenuArgs) {
 		}
 	);
 
-	const trigger = elementDerived([open, options], ([$open, $options], { attach }) => {
+	const subTrigger = elementDerived([open, options], ([$open, $options], { attach }) => {
 		attach('pointerdown', (e) => {
 			e.stopImmediatePropagation();
 		});
@@ -385,7 +322,16 @@ export function createSubMenu(args?: CreateSubMenuArgs) {
 		});
 
 		attach('mouseover', (e) => {
-			(e.currentTarget as HTMLElement).focus();
+			const triggerEl = e.currentTarget as HTMLElement;
+
+			open.update((prev) => {
+				const isOpen = prev;
+				if (!isOpen) {
+					activeTrigger.set(triggerEl);
+					return !prev;
+				}
+				return prev;
+			});
 		});
 
 		attach('mouseout', (e) => {
@@ -401,7 +347,7 @@ export function createSubMenu(args?: CreateSubMenuArgs) {
 			'data-state': $open ? 'open' : 'closed',
 			'data-disabled': $options.disabled ? '' : undefined,
 			id: ids.trigger,
-			tabindex: 0,
+			tabindex: -1,
 		};
 	});
 
@@ -414,127 +360,20 @@ export function createSubMenu(args?: CreateSubMenuArgs) {
 		}),
 	}));
 
-	type ItemArgs =
-		| {
-				id: string;
-		  }
-		| string
-		| number;
-
-	let typed: string[] = [];
-	const resetTyped = debounce(() => {
-		typed = [];
-	});
-
 	effect([parentOpen], ([$parentOpen]) => {
 		if (!$parentOpen) {
 			open.set(false);
+			activeTrigger.set(null);
 		}
-	});
-
-	effect([open, menu, activeTrigger], ([$open, $menu, $activeTrigger]) => {
-		if (!isBrowser) return;
-
-		const menuEl = getElementByMeltId($menu['data-melt-id']);
-		if (menuEl && $open) {
-			// Focus on selected option or first option
-			const selectedOption = menuEl.querySelector('[data-selected]') as HTMLElement | undefined;
-			if (!selectedOption) {
-				const firstOption = menuEl.querySelector('[role="option"]') as HTMLElement | undefined;
-				sleep(1).then(() => firstOption?.focus());
-			} else {
-				sleep(1).then(() => selectedOption.focus());
-			}
-
-			const keydownListener = (e: KeyboardEvent) => {
-				if (e.key === kbd.ESCAPE) {
-					open.set(false);
-					activeTrigger.set(null);
-					return;
-				}
-
-				const allOptions = Array.from(menuEl.querySelectorAll('[role="option"]')) as HTMLElement[];
-				const focusedOption = allOptions.find((el) => el === document.activeElement);
-				const focusedIndex = allOptions.indexOf(focusedOption as HTMLElement);
-
-				if (e.key === kbd.ARROW_DOWN) {
-					e.preventDefault();
-					const nextIndex = focusedIndex + 1 > allOptions.length - 1 ? 0 : focusedIndex + 1;
-					const nextOption = allOptions[nextIndex] as HTMLElement;
-					nextOption.focus();
-					return;
-				} else if (e.key === kbd.ARROW_UP) {
-					e.preventDefault();
-					const prevIndex = focusedIndex - 1 < 0 ? allOptions.length - 1 : focusedIndex - 1;
-					const prevOption = allOptions[prevIndex] as HTMLElement;
-					prevOption.focus();
-					return;
-				} else if (e.key === kbd.HOME) {
-					e.preventDefault();
-					const firstOption = allOptions[0] as HTMLElement;
-					firstOption.focus();
-					return;
-				} else if (e.key === kbd.END) {
-					e.preventDefault();
-					const lastOption = allOptions[allOptions.length - 1] as HTMLElement;
-					lastOption.focus();
-					return;
-				}
-
-				// Typeahead
-				const isAlphaNumericOrSpace = /^[a-z0-9 ]$/i.test(e.key);
-				if (isAlphaNumericOrSpace) {
-					typed.push(e.key.toLowerCase());
-					const typedString = typed.join('');
-					const matchingOption = allOptions.find((el) =>
-						el.innerText.toLowerCase().startsWith(typedString)
-					);
-					if (matchingOption) {
-						matchingOption.focus();
-					}
-
-					resetTyped();
-				}
-			};
-
-			document.addEventListener('keydown', keydownListener);
-			return () => {
-				document.removeEventListener('keydown', keydownListener);
-			};
-		} else if (!$open && $activeTrigger && isBrowser) {
-			// Hacky way to prevent the keydown event from triggering on the trigger
-			sleep(1).then(() => $activeTrigger.focus());
-		}
-	});
-
-	const input = derived([selected, options], ([$selected, $options]) => {
-		return {
-			type: 'hidden',
-			name: $options.name,
-			value: $selected,
-			'aria-hidden': true,
-			hidden: true,
-			tabIndex: -1,
-			required: $options.required,
-			disabled: $options.disabled,
-			style: styleToString({
-				position: 'absolute',
-				opacity: 0,
-				'pointer-events': 'none',
-				margin: 0,
-				transform: 'translateX(-100%)',
-			}),
-		};
 	});
 
 	return {
-		subTrigger: trigger,
-		subMenu: menu,
+		subTrigger,
+		subMenu,
 		open,
 		selected,
 		selectedText,
 		arrow,
 		options,
-		input,
 	};
 }
