02a33315e3c5f64c212171cfcee08d0b7c632589
tglide <26071571+TGlide@users.noreply.github.com>
2023-06-20 11:42:07 +0100

    fix: docs tabs arrow keys not working

--- src/lib/builders/tabs/index.ts
+++ src/lib/builders/tabs/index.ts
@@ -99,6 +99,7 @@ export function createTabs(args?: CreateTabsArgs) {
 			attach('keydown', (e) => {
 				const el = e.currentTarget as HTMLElement;
 				const rootEl = el.closest('[data-melt-part="tabs-root"]') as HTMLElement | null;
+				console.log(rootEl);
 				if (!rootEl) return;
 
 				const triggers = Array.from(rootEl.querySelectorAll('[role="tab"]')) as HTMLElement[];
@@ -108,6 +109,7 @@ export function createTabs(args?: CreateTabsArgs) {
 				const dir = getElemDirection(rootEl);
 				const { nextKey, prevKey } = getDirectionalKeys(dir, $options.orientation);
 
+				console.log(nextKey, prevKey);
 				if (e.key === nextKey) {
 					e.preventDefault();
 					next(enabledTriggers, triggerIdx, $options.loop)?.focus();
