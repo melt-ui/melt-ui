f34b34d9d142a7939af2a0d739deca1bb34145f3
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-23 17:11:19 -0400

    dialog docs

--- src/routes/docs/builders/dialog/+page.svelte
+++ src/routes/docs/builders/dialog/+page.svelte
@@ -2,6 +2,11 @@
 	import { Docs } from '$routes/(components)';
 
 	export let data;
+	const features = [
+		'Fully managed focus',
+		'Can be controlled or uncontrolled',
+		'Esc closes the component automaticlaly',
+	];
 </script>
 
 <Docs.H1>Dialog</Docs.H1>
@@ -11,6 +16,61 @@
 </Docs.Description>
 
 <Docs.Preview {...data.dialog} />
+<Docs.Construction />
+
+<Docs.Features {features} />
+
+<Docs.H2>Anatomy</Docs.H2>
+<Docs.Ul>
+	<Docs.Li><b>Trigger:</b> The button(s) that open the dialog</Docs.Li>
+	<Docs.Li><b>Overlay:</b> The dim background that is typically behind a dialog element.</Docs.Li>
+	<Docs.Li
+		><b>Content:</b> Container for the content within the dialog.
+		<Docs.Ul>
+			<Docs.Li><b>Title:</b>The title of the dialog</Docs.Li>
+			<Docs.Li>
+				<b>Description:</b> The description which supports the title
+			</Docs.Li>
+			<Docs.Li><b>Close:</b> The button(s) that close the dialog</Docs.Li>
+		</Docs.Ul>
+	</Docs.Li>
+</Docs.Ul>
+
+<Docs.H2>Usage</Docs.H2>
+<Docs.P>
+	To create a dialog, use the <Docs.Code>createDialog</Docs.Code> builder function. You can then reference
+	the anatomy or example above to create your dialog.
+</Docs.P>
+
+<Docs.H2>API Reference</Docs.H2>
+<Docs.API
+	schema={{
+		title: 'CreateDialogArgs',
+		description: 'The object you pass into createAccordion. Optional.',
+		args: [
+			{
+				label: 'role',
+				type: "'dialog' | 'alert-dialog'",
+				default: "'dialog'",
+			},
+			{
+				label: 'preventScroll',
+				type: 'boolean',
+				default: true,
+			},
+			{
+				label: 'closeOnEscape',
+				type: 'boolean',
+				default: true,
+			},
+			{
+				label: 'closeOnOutsideClick',
+				type: 'boolean',
+				default: true,
+			},
+		],
+	}}
+/>
 
 <Docs.H2>Example Components</Docs.H2>
 <Docs.H3>Drawer</Docs.H3>
@@ -22,4 +82,41 @@
 
 <Docs.Preview {...data.drawer} />
 
-<Docs.Construction />
+<Docs.H2>Accessibility</Docs.H2>
+<Docs.P>
+	Adheres to the
+	<Docs.A href="https://www.w3.org/WAI/ARIA/apg/patterns/dialog/"
+		>Dialog WAI-ARIA design pattern</Docs.A
+	> & <Docs.A href="https://www.w3.org/WAI/ARIA/apg/patterns/alertdialog/"
+    >Alert Dialog WAI-ARIA design pattern</Docs.A
+>
+</Docs.P>
+
+<Docs.API
+	schema={{
+		title: 'Keyboard Interactions',
+		description: '',
+		keyboardInteractions: [
+			{
+				key: 'Space',
+				description: 'Opens/closes the dialog.',
+			},
+			{
+				key: 'Enter',
+				description: 'Opens/closes the dialog.',
+			},
+			{
+				key: 'Tab',
+				description: 'Moves focus to the next focusable element within the dialog.',
+			},
+			{
+				key: 'Shift + Tab',
+				description: 'Moves focus to the previous focusable element within the dialog.',
+			},
+			{
+				key: 'Esc',
+				description: 'Closes the dialog and moves focus to the trigger element.',
+			},
+		],
+	}}
+/>
