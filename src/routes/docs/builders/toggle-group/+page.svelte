fc1b935fc08cdc718b0ea8e38546b6d15c49ef77
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-24 14:07:44 -0400

    initial toggle group docs

--- src/routes/docs/builders/toggle-group/+page.svelte
+++ src/routes/docs/builders/toggle-group/+page.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
 	import { Docs } from '$routes/(components)';
+	import { schemas } from './schemas.js';
 
 	export let data;
+	const features = [
+		'Horizontal or vertical orientation',
+		'Can be controlled or uncontrolled',
+		'Full keyboard navigation',
+	];
 </script>
 
 <Docs.H1>Toggle Group</Docs.H1>
 <Docs.Description>A set of two-state buttons that can be toggled on or off.</Docs.Description>
 
 <Docs.Preview {...data.preview} />
+
+<Docs.Features {features} />
+
+<Docs.H2>Anatomy</Docs.H2>
+<Docs.Ul>
+	<Docs.Li><b>Root:</b> The toggle group container component</Docs.Li>
+	<Docs.Li><b>Item:</b> A toggle group item component</Docs.Li>
+</Docs.Ul>
+
+<Docs.H2>API Reference</Docs.H2>
+<Docs.API schema={schemas.builder} />
+<Docs.API schema={schemas.root} />
+
+<Docs.H2>Accessibility</Docs.H2>
+<Docs.P>
+	Adheres to the
+	<Docs.A href="https://www.w3.org/WAI/ARIA/apg/patterns/button/"
+		>Button WAI-ARIA design pattern</Docs.A
+	>
+</Docs.P>
+
+<Docs.API schema={schemas.keyboard} />
+
 
 <Docs.Construction />
---
+++ src/routes/docs/builders/toggle-group/schemas.ts
@@ -0,0 +1,133 @@
+import type { APISchema } from '$routes/(components)';
+
+const builder: APISchema = {
+	title: 'CreateToggleGroupArgs',
+	description: 'Configuration options for the `createToggleGroup` builder.',
+	args: [
+		{
+			label: 'type',
+			type: ["'single'", "'multiple'"],
+			default: "'single'",
+		},
+		{
+			label: 'disabled',
+			type: 'boolean',
+			default: false,
+		},
+		{
+			label: 'value',
+			type: ["'string'", 'string[]', 'null'],
+			default: 'null',
+		},
+		{
+			label: 'rovingFocus',
+			type: 'boolean',
+			default: true,
+		},
+		{
+			label: 'orientation',
+			type: ["'horizontal'", "'vertical'"],
+			default: "'horizontal'",
+		},
+		{
+			label: 'loop',
+			type: 'boolean',
+			default: true,
+		},
+	],
+};
+
+const root: APISchema = {
+	title: 'Root',
+	description: 'The root toggle group element.',
+	dataAttributes: [
+		{
+			label: 'data-orientation',
+			value: ["'horizontal'", "'vertical'"],
+		},
+		{
+			label: 'data-melt-part',
+			value: '`toggle-group`',
+		},
+	],
+};
+
+const item: APISchema = {
+	title: 'Item',
+	description: 'The toggle group item element.',
+	args: [
+		{
+			label: 'value',
+			type: 'string',
+		},
+		{
+			label: 'disabled',
+			type: 'boolean',
+			default: false,
+		},
+	],
+	dataAttributes: [
+		{
+			label: 'data-orientation',
+			value: ["'horizontal'", "'vertical'"],
+		},
+		{
+			label: 'data-melt-part',
+			value: '`toggle-group-item`',
+		},
+		{
+			label: 'data-state',
+			value: ["'on'", "'off'"],
+		},
+	],
+};
+
+const keyboard: APISchema = {
+	title: 'Keyboard Interactions',
+	description: '',
+	keyboardInteractions: [
+		{
+			key: 'Tab',
+			description: 'Moves focus to either the pressed item or the first item in the group.',
+		},
+		{
+			key: 'Space',
+			description: 'Activates/deactivates the item.',
+		},
+		{
+			key: 'Enter',
+			description: 'Activates/deactivates the item.',
+		},
+		{
+			key: 'ArrowDown',
+			description: 'Moves focus to the next item in the group.',
+		},
+		{
+			key: 'ArrowRight',
+			description: 'Moves focus to the next item in the group.',
+		},
+		{
+			key: 'ArrowUp',
+			description: 'Moves focus to the previous item in the group.',
+		},
+		{
+			key: 'ArrowLeft',
+			description: 'Moves focus to the previous item in the group.',
+		},
+		{
+			key: 'Home',
+			description: 'Moves focus to the first item in the group.',
+		},
+		{
+			key: 'End',
+			description: 'Moves focus to the last item in the group.',
+		},
+	],
+};
+
+export const schemas = {
+	builder,
+	root,
+	item,
+	keyboard,
+};
