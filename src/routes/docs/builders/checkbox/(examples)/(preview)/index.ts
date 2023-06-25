6f4b69c78701d71c85362c3136dd26b3d7585db8
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-24 21:04:57 -0400

    fix globals ssr issue

--- src/routes/docs/builders/checkbox/(examples)/(preview)/index.ts
+++ src/routes/docs/builders/checkbox/(examples)/(preview)/index.ts
@@ -2,7 +2,7 @@ import type { PreviewProps } from '$routes/(components)';
 import tw from './tailwind.ignore.svelte?raw';
 import css from './css.ignore.svelte?raw';
 import TwConfig from '$routes/(other)/tailwind.config.ts?raw';
-import GlobalReset from '$routes/(other)/globals.css?raw';
+import GlobalReset from '$routes/(other)/globals?raw';
 
 export { default as Preview } from './preview.svelte';
 export const Tailwind: PreviewProps['code']['Tailwind'] = {
