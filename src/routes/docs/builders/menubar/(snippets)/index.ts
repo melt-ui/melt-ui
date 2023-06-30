b124554d2c8b2062540ff3a3b274fe6347d104a8
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-29 23:00:41 -0400

    feat(menubar): initial docs

--- src/routes/docs/builders/menubar/(snippets)/index.ts
+++ src/routes/docs/builders/menubar/(snippets)/index.ts
@@ -1,11 +1,9 @@
+import usage from './usage.ignore-svelte?raw';
 import usage2 from './usage-2.ignore-svelte?raw';
 import usage3 from './usage-3.ignore-svelte?raw';
-import usage4 from './usage-4.ignore-svelte?raw';
-import usage from './usage.ignore-svelte?raw';
 
 export const snippets = {
 	usage,
 	usage2,
 	usage3,
-	usage4,
 };
--- src/routes/docs/builders/menubar/schemas.ts
+++ src/routes/docs/builders/menubar/schemas.ts
@@ -1,8 +1,22 @@
 import type { APISchema } from '$routes/(components)/api.svelte';
+import { schemas as menuSchemas } from '$routes/docs/builders/dropdown-menu/schemas';
 
 const builder: APISchema = {
-	title: 'CreateDropdownMenuArgs',
-	description: 'The configuration object passed to the `createDropdownMenu` builder function.',
+	title: 'CreateMenubarArgs',
+	description: 'The configuration object passed to the `createMenubar` builder function.',
+	args: [
+		{
+			label: 'loop',
+			type: 'boolean',
+			default: 'true',
+		},
+	],
+};
+
+const menuBuilder: APISchema = {
+	title: 'CreateMenuArgs',
+	description:
+		'The configuration object passed to the `createMenu` builder function returned from the `createMenubar` builder function.',
 	args: [
 		{
 			label: 'positioning',
@@ -19,218 +33,10 @@ const builder: APISchema = {
 			type: 'boolean',
 			default: true,
 		},
-	],
-};
-
-const menu: APISchema = {
-	title: 'Menu',
-	description: 'The element which wraps the entire dropdown menu.',
-	dataAttributes: [
 		{
-			label: '[data-melt-part]',
-			value: ['"menu"'],
-		},
-		{
-			label: '[data-state]',
-			value: ['"open"', '"closed"'],
-		},
-		{
-			label: '[data-melt-menu]',
-			value: 'Present on the menu element.',
-		},
-	],
-};
-
-const trigger: APISchema = {
-	title: 'Trigger',
-	description: 'The button which toggles the dropdown menu.',
-	dataAttributes: [
-		{
-			label: '[data-melt-part]',
-			value: ['"trigger"'],
-		},
-		{
-			label: '[data-state]',
-			value: ['"open"', '"closed"'],
-		},
-	],
-};
-
-const arrow: APISchema = {
-	title: 'Arrow',
-	description: 'An optional arrow element which points to the trigger.',
-	dataAttributes: [
-		{
-			label: '[data-arrow]',
-			value: '`true`',
-		},
-		{
-			label: '[data-melt-part]',
-			value: ['"arrow"'],
-		},
-	],
-};
-
-const item: APISchema = {
-	title: 'Item',
-	description: 'A basic menu item.',
-	args: [
-		{
-			label: 'onSelect',
-			type: 'function',
-		},
-	],
-	dataAttributes: [
-		{
-			label: '[data-orientation]',
-			value: ['"vertical"', '"horizontal"'],
-		},
-		{
-			label: '[data-melt-part]',
-			value: ['"item"'],
-		},
-	],
-};
-
-const checkboxItem: APISchema = {
-	title: 'Checkbox Item',
-	description: 'A checkbox menu item.',
-	args: [
-		{
-			label: 'checked',
-			type: 'Writable<boolean>',
-		},
-		{
-			label: 'onSelect',
-			type: 'function',
-		},
-	],
-	dataAttributes: [
-		{
-			label: '[data-orientation]',
-			value: ['"vertical"', '"horizontal"'],
-		},
-		{
-			label: '[data-melt-part]',
-			value: ['"item"'],
-		},
-	],
-};
-
-const radioGroupBuilder = {
-	title: 'CreateMenuRadioGroupArgs',
-	description: 'The configuration object passed to the `createMenuRadioGroup` builder function.',
-	args: [
-		{
-			label: 'value',
-			type: 'string',
-		},
-	],
-};
-
-const radioGroup: APISchema = {
-	title: 'Menu Radio Group',
-	description: 'A group of radio menu items.',
-	dataAttributes: [
-		{
-			label: '[data-melt-part]',
-			value: ['"radio-group"'],
-		},
-	],
-};
-
-const radioItem: APISchema = {
-	title: 'Radio Group Item',
-	description: 'A radiogroup menu item.',
-	args: [
-		{
-			label: 'value',
-			type: 'string',
-		},
-		{
-			label: 'disabled',
+			label: 'loop',
 			type: 'boolean',
-			default: 'false',
-		},
-		{
-			label: 'onSelect',
-			type: 'function',
-		},
-	],
-	dataAttributes: [
-		{
-			label: '[data-orientation]',
-			value: ['"vertical"', '"horizontal"'],
-		},
-		{
-			label: '[data-melt-part]',
-			value: ['"item"'],
-		},
-	],
-};
-
-const separator: APISchema = {
-	title: 'Separator',
-	description: 'A horizontal line which separates menu items.',
-	dataAttributes: [
-		{
-			label: '[data-melt-part]',
-			value: ['"separator"'],
-		},
-	],
-};
-
-const submenuBuilder: APISchema = {
-	title: 'CreateDropdownSubMenuArgs',
-	description: 'The configuration object passed to the `createDropdownSubMenu` builder function.',
-	args: [
-		{
-			label: 'positioning',
-			type: 'FloatingConfig',
-			default: "placement: 'right'",
-		},
-		{
-			label: 'disabled',
-			type: 'boolean',
-			default: 'false',
-		},
-	],
-};
-
-const submenu: APISchema = {
-	title: 'Submenu',
-	description: 'A submenu element displayed when its trigger is selected.',
-	dataAttributes: [
-		{
-			label: '[data-melt-part]',
-			value: ['"submenu"'],
-		},
-		{
-			label: '[data-state]',
-			value: ['"open"', '"closed"'],
-		},
-		{
-			label: '[data-melt-menu]',
-			value: 'Present on the submenu element.',
-		},
-	],
-};
-
-const subTrigger: APISchema = {
-	title: 'Sub Trigger',
-	description: 'A button which opens its associated submenu.',
-	dataAttributes: [
-		{
-			label: '[data-melt-part]',
-			value: ['"subtrigger"'],
-		},
-		{
-			label: '[data-state]',
-			value: ['"open"', '"closed"'],
-		},
-		{
-			label: '[data-disabled]',
-			value: 'Present if the element is disabled',
+			default: false,
 		},
 	],
 };
@@ -242,17 +48,17 @@ const keyboard: APISchema = {
 		{
 			key: 'Space',
 			description:
-				'When focused on the `trigger`, opens the dropdown and focuses the first item. When focused on an `item`, selects the item.',
+				'When focused on the `trigger`, opens the associated menu. When focused on an `item`, selects the item.',
 		},
 		{
 			key: 'Enter',
 			description:
-				'When focused on the `trigger`, opens the dropdown and focuses the first item. When focused on an `item`, selects the item.',
+				'When focused on the `trigger`, opens the associated menu. When focused on an `item`, selects the item.',
 		},
 		{
 			key: 'ArrowDown',
 			description:
-				'When focused on the `trigger`, opens the dropdown and focuses the first item. When focused on an `item`, shifts focus to the next item.',
+				'When focused on a `trigger`, opens the associated menu. When focused on an `item`, shifts focus to the next item.',
 		},
 		{
 			key: 'ArrowUp',
@@ -261,33 +67,34 @@ const keyboard: APISchema = {
 		{
 			key: 'ArrowRight',
 			description:
-				'When focused on a `subTrigger`, opens the `subMenu` and focuses the first item.',
+				'When focused on a `subTrigger`, opens the `subMenu` and focuses the first item. When focus is within the menu, opens the next menu in the menubar',
 		},
 		{
 			key: 'ArrowLeft',
 			description:
-				"When focused on within a `subMenu`, closes the submenu and shifts focus to that submenu's `subTrigger`.",
+				"When focused on within a `subMenu`, closes the submenu and shifts focus to that submenu's `subTrigger`. When focus is within the menu, opens the previous menu in the menubar",
 		},
 		{
 			key: 'Esc',
-			description: 'Closes the dropdown menu and focuses the `trigger`.',
+			description: "Closes the open menu and focuses that menu's `trigger`.",
 		},
 	],
 };
 
 export const schemas = {
 	builder,
-	radioGroup,
-	radioItem,
-	radioGroupBuilder,
-	checkboxItem,
-	menu,
-	arrow,
-	item,
-	trigger,
+	menuBuilder,
+	radioGroup: menuSchemas.radioGroup,
+	radioItem: menuSchemas.radioItem,
+	radioGroupBuilder: menuSchemas.radioGroupBuilder,
+	checkboxItem: menuSchemas.checkboxItem,
+	menu: menuSchemas.menu,
+	arrow: menuSchemas.arrow,
+	item: menuSchemas.item,
+	trigger: menuSchemas.trigger,
 	keyboard,
-	subTrigger,
-	submenu,
-	submenuBuilder,
-	separator,
+	subTrigger: menuSchemas.subTrigger,
+	submenu: menuSchemas.submenu,
+	submenuBuilder: menuSchemas.submenuBuilder,
+	separator: menuSchemas.separator,
 };
