ea5c9ce8202b400f0c79df2c461b11fa6dfdb3d5
Michael Bell <michaelrbell@ymail.com>
2023-07-05 13:03:45 +0100

    tags-input: update to use new style refactor

--- src/routes/docs/builders/tags-input/(examples)/(preview)/tailwind.svelte
+++ src/routes/docs/builders/tags-input/(examples)/(preview)/tailwind.svelte
@@ -2,21 +2,22 @@
 	import { createTagsInput } from '@melt-ui/svelte';
 	import X from '~icons/lucide/x';
 
-	const { root, input, tags, tag, deleteTrigger } = createTagsInput({
+	const { root, input, tags, tag, deleteTrigger, edit } = createTagsInput({
 		tags: ['Svelte', 'Typescript'],
+		unique: true,
 	});
 </script>
 
 <div class="flex flex-col items-start justify-center gap-2">
 	<div
 		{...$root}
-		use:root
 		class="flex min-w-[280px] flex-row flex-wrap gap-2.5 rounded-md bg-white px-3 py-2 text-magnum-700
 		focus-within:ring focus-within:ring-magnum-400"
 	>
 		{#each $tags as t}
 			<div
 				{...$tag(t)}
+				use:tag
 				class="flex items-center overflow-hidden rounded-md bg-magnum-200 text-magnum-900 [word-break:break-word]
 			data-[disabled]:bg-magnum-300 data-[selected]:bg-magnum-400 data-[disabled]:hover:cursor-default
 				data-[disabled]:focus:!outline-none data-[disabled]:focus:!ring-0"
@@ -29,6 +30,13 @@
 				>
 					<X class="h-3 w-3" />
 				</button>
+			</div>
+			<div
+				{...$edit(t)}
+				use:edit
+				class="flex items-center overflow-hidden rounded-md px-1.5 [word-break:break-word] data-[invalid-edit]:focus:!ring-red-500"
+			>
+				{t.value}
 			</div>
 		{/each}
 
