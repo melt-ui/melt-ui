c4beff6ca0f777a3f6682e9759d50fdc25d3d4a3
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-05 17:03:38 -0500

    replace spaces with dashes

--- src/routes/docs/[...slug]/+page.ts
+++ src/routes/docs/[...slug]/+page.ts
@@ -6,7 +6,7 @@ import { navConfig } from '$docs/config';
 
 export const entries = (() => {
 	return navConfig.sidebarNav[0].items.map((item) => {
-		return { slug: item.title.toLowerCase() };
+		return { slug: item.title.toLowerCase().replaceAll(' ', '-') };
 	});
 }) satisfies EntryGenerator;
 
