c1315794481c96b4e1010dc67f6882966114f60a
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-05 09:44:41 -0400

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from '@huntabyte/mdsvex';
 import { mdsvexOptions } from './mdsvex.config.js';
