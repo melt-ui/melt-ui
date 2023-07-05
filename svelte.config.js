53dc9af6c84954d352d3fad735370e11d16e5688
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-05 10:09:58 -0400

    adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from '@huntabyte/mdsvex';
 import { mdsvexOptions } from './mdsvex.config.js';
@@ -14,7 +14,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs18.x',
+		}),
 		alias: {
 			$routes: 'src/routes',
 			'$routes/*': 'src/routes/*',
