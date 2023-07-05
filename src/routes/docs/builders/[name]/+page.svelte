93161f03676cc6dcfe36ae4cc790265f06597704
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-05 10:22:54 -0400

    key the docs

--- src/routes/docs/builders/[name]/+page.svelte
+++ src/routes/docs/builders/[name]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { TOC, Description, Preview, Features } from '$docs/components';
-
 	import type { SvelteComponent } from 'svelte';
 	import type { PageData } from './$types';
 	import { cn } from '$docs/utils';
@@ -19,35 +18,37 @@
 	$: builderData = data.builderData;
 </script>
 
-<main class="relative px-2 py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_240px]">
-	<div class="mx-auto w-full min-w-0">
-		<div class="space-y-2">
-			<h1 class={cn('scroll-m-20 text-4xl font-bold tracking-tight')}>
-				{doc.title}
-			</h1>
-			{#if doc.description}
-				<Description>
-					{doc.description}
-				</Description>
-			{/if}
-		</div>
-		<div {...$separator} class="my-4 md:my-6" />
-		<div class="mdsvex" id="mdsvex">
-			<Preview code={snippets.main} fullwidth>
-				<svelte:component this={mainPreview} />
-			</Preview>
-			<Features features={builderData.features} />
+{#key $page.url.pathname}
+	<main class="relative px-2 py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_240px]">
+		<div class="mx-auto w-full min-w-0">
+			<div class="space-y-2">
+				<h1 class={cn('scroll-m-20 text-4xl font-bold tracking-tight')}>
+					{doc.title}
+				</h1>
+				{#if doc.description}
+					<Description>
+						{doc.description}
+					</Description>
+				{/if}
+			</div>
+			<div {...$separator} class="my-4 md:my-6" />
+			<div class="mdsvex" id="mdsvex">
+				<Preview code={snippets.main} fullwidth>
+					<svelte:component this={mainPreview} />
+				</Preview>
+				<Features features={builderData.features} />
 
-			<svelte:component this={component} {snippets} />
+				<svelte:component this={component} {snippets} />
+			</div>
+			<div {...$separator} class="my-4 md:my-6" />
+			<!-- <DocsPager /> -->
 		</div>
-		<div {...$separator} class="my-4 md:my-6" />
-		<!-- <DocsPager /> -->
-	</div>
-	<div class="hidden text-sm xl:block">
-		<div class="sticky top-16 -mt-10 h-[calc(100vh-3.5rem)] overflow-hidden pt-6">
-			{#key $page.url.pathname}
-				<TOC />
-			{/key}
+		<div class="hidden text-sm xl:block">
+			<div class="sticky top-16 -mt-10 h-[calc(100vh-3.5rem)] overflow-hidden pt-6">
+				{#key $page.url.pathname}
+					<TOC />
+				{/key}
+			</div>
 		</div>
-	</div>
-</main>
+	</main>
+{/key}
