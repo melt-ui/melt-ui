8f6778142f277146a5dabc6f4cc40768b1900550
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-06 18:22:28 -0400

    added PP to pin-input

--- src/routes/docs/builders/pin-input/(examples)/(preview)/tailwind.svelte
+++ src/routes/docs/builders/pin-input/(examples)/(preview)/tailwind.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import { createPinInput } from '@melt-ui/svelte';
+	import { createPinInput, melt } from '@melt-ui/svelte';
 
 	const { root, input } = createPinInput();
 </script>
 
-<div {...$root} class="flex items-center gap-2">
+<div use:melt={$root} class="flex items-center gap-2">
 	{#each Array.from({ length: 6 }) as _, i}
 		<input
 			class="rounded-md bg-white text-center text-lg text-magnum-900 shadow-sm square-12"
-			{...$input}
-			use:input
+			use:melt={$input}
 		/>
 	{/each}
 </div>
