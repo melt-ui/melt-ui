42b98cb13ddc7acdc50bf24e0f7f1dbd6c2ce44e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-06 18:01:57 -0400

    added PP to collapsible

--- src/routes/docs/builders/collapsible/(examples)/(preview)/tailwind.svelte
+++ src/routes/docs/builders/collapsible/(examples)/(preview)/tailwind.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { createCollapsible } from '@melt-ui/svelte';
+	import { createCollapsible, melt } from '@melt-ui/svelte';
 	import { slide } from 'svelte/transition';
 	import ChevronsUpDown from '~icons/lucide/ChevronsUpDown';
 	import X from '~icons/lucide/x';
@@ -7,12 +7,11 @@
 	const { open, root, content, trigger } = createCollapsible();
 </script>
 
-<div {...$root} class="mx-auto w-full max-w-md">
+<div use:melt={$root} class="mx-auto w-full max-w-md">
 	<div class="flex items-center justify-between">
 		<span class="text-sm leading-6 text-white"> @thomasglopes starred 3 repositories </span>
 		<button
-			{...$trigger}
-			use:trigger
+			use:melt={$trigger}
 			class="relative h-6 w-6 place-items-center rounded-full bg-white text-sm text-magnum-700
         shadow-lg hover:opacity-75
         data-[disabled]:cursor-not-allowed data-[disabled]:opacity-75"
@@ -32,7 +31,7 @@
 	</div>
 
 	{#if $open}
-		<div {...$content} transition:slide>
+		<div use:melt={$content} transition:slide>
 			<div class="flex flex-col gap-2">
 				<div class="rounded bg-white p-3 shadow-lg">
 					<span class="text-base leading-[25px] text-magnum-800">sveltejs/svelte</span>
