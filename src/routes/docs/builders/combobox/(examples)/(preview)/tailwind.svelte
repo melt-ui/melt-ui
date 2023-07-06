28316b1cd879b160261578cbcf9097bd8cc6a5f2
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-06 18:06:23 -0400

    adds PP to combobox

--- src/routes/docs/builders/combobox/(examples)/(preview)/tailwind.svelte
+++ src/routes/docs/builders/combobox/(examples)/(preview)/tailwind.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { createCombobox } from '@melt-ui/svelte';
+	import { createCombobox, melt } from '@melt-ui/svelte';
 	import Check from '~icons/lucide/check';
 	import ChevronDown from '~icons/lucide/chevron-down';
 	import ChevronUp from '~icons/lucide/chevron-up';
@@ -43,10 +43,9 @@
 	<span class="block pb-1 capitalize">Choose your favorite book:</span>
 	<div class="relative">
 		<input
-			{...$input}
+			use:melt={$input}
 			class="flex h-10 items-center justify-between rounded-md bg-white px-3 pr-12 text-magnum-700"
 			placeholder="Best book ever"
-			use:input
 			value={$inputValue}
 		/>
 		<div class="absolute right-1 top-1/2 z-10 -translate-y-1/2 text-magnum-700">
@@ -59,14 +58,13 @@
 	</div>
 </label>
 
-<div class="z-10 flex max-h-[300px] flex-col overflow-hidden rounded-md" {...$menu} use:menu>
+<div class="z-10 flex max-h-[300px] flex-col overflow-hidden rounded-md" use:melt={$menu}>
 	<ul class="flex max-h-full flex-col gap-2 overflow-y-auto bg-white px-2 py-2">
 		{#if $open}
 			{#if $filteredItems.length !== 0}
 				{#each $filteredItems as book, index (index)}
 					<li
-						{...$item({ index, item: book, disabled: book.disabled })}
-						use:item
+						use:melt={$item({ index, item: book, disabled: book.disabled })}
 						class="relative cursor-pointer rounded-md py-1 pl-8 pr-4 text-neutral-800
 						data-[highlighted]:bg-magnum-100 data-[highlighted]:text-magnum-700 data-[disabled]:opacity-50"
 					>
