303cd3cb701527a5e821f1dae65a4cb6ffbfe265
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-10 19:37:25 -0400

    fix: add breathing room to keys

--- src/docs/components/tables/kbd-table.svelte
+++ src/docs/components/tables/kbd-table.svelte
@@ -13,12 +13,14 @@
 				<table class="w-full min-w-[540px] border-b border-neutral-700 text-left sm:min-w-full">
 					<tbody class="divide-y divide-neutral-700">
 						<tr class="w-full text-neutral-300">
-							<td class="w-1/6 whitespace-nowrap py-2 pl-4 text-sm font-medium sm:pl-0"> Key </td>
+							<td class="w-1/6 whitespace-nowrap py-2 pl-4 pr-4 text-sm font-medium sm:pl-0">
+								Key
+							</td>
 							<td class="w-5/6 whitespace-nowrap py-2 text-sm font-medium">Behavior</td>
 						</tr>
 						{#each keyboard as { key, behavior }}
 							<tr>
-								<td class="py-3 pl-4 align-baseline sm:pl-0">
+								<td class="py-3 pl-4 pr-4 align-baseline sm:pl-0">
 									<Kbd>{key}</Kbd>
 								</td>
 								<td class="py-3 align-baseline text-sm">
