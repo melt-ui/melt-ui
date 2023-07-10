628c8fe7ea7411c47bf4c80376a9a56ad017eecf
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-10 18:59:31 -0400

    update tags input preview to latest

--- src/docs/previews/tags-input/main/tailwind.svelte
+++ src/docs/previews/tags-input/main/tailwind.svelte
@@ -2,50 +2,48 @@
 	import { createTagsInput } from '@melt-ui/svelte';
 	import X from '~icons/lucide/x';
 
-	const { root, input, tags, tag, deleteTrigger } = createTagsInput({
-		tags: ['one', 'two'],
-		placeholder: 'Enter tags...',
+	const { root, input, tags, tag, deleteTrigger, edit } = createTagsInput({
+		tags: ['Svelte', 'Typescript'],
+		unique: true,
 	});
 </script>
 
-<div
-	{...$root}
-	use:root
-	class="flex w-[280px] flex-row flex-wrap gap-2.5 rounded-md bg-white
-    px-3 py-2 text-magnum-700"
->
-	{#each $tags as t}
-		<div {...$tag(t)} class="tag">
-			<span class="flex items-center border-r border-white/10 px-1.5"
-				>{t.value}</span
+<div class="flex flex-col items-start justify-center gap-2">
+	<div
+		{...$root}
+		use:root
+		class="flex min-w-[280px] flex-row flex-wrap gap-2.5 rounded-md bg-white px-3 py-2 text-magnum-700
+		focus-within:ring focus-within:ring-magnum-400"
+	>
+		{#each $tags as t}
+			<div
+				{...$tag(t)}
+				use:tag
+				class="flex items-center overflow-hidden rounded-md bg-magnum-200 text-magnum-900 [word-break:break-word]
+			data-[disabled]:bg-magnum-300 data-[selected]:bg-magnum-400 data-[disabled]:hover:cursor-default
+				data-[disabled]:focus:!outline-none data-[disabled]:focus:!ring-0"
 			>
+				<span class="flex items-center border-r border-white/10 px-1.5">{t.value}</span>
+				<button
+					{...$deleteTrigger(t)}
+					use:deleteTrigger
+					class="flex h-full items-center px-1 enabled:hover:bg-magnum-300"
+				>
+					<X class="h-3 w-3" />
+				</button>
+			</div>
+			<div
+				{...$edit(t)}
+				use:edit
+				class="flex items-center overflow-hidden rounded-md px-1.5 [word-break:break-word] data-[invalid-edit]:focus:!ring-red-500"
+			/>
+		{/each}
 
-			<button {...$deleteTrigger(t)} use:deleteTrigger class="tag-delete">
-				<X class="h-3 w-3" />
-			</button>
-		</div>
-	{/each}
-
-	<input
-		{...$input}
-		use:input
-		type="text"
-		class="shake min-w-[4.5rem] shrink grow basis-0 border-0 outline-none focus:!ring-0"
-	/>
+		<input
+			{...$input}
+			use:input
+			type="text"
+			class="min-w-[4.5rem] shrink grow basis-0 border-0 text-black outline-none focus:!ring-0 data-[invalid]:text-red-500"
+		/>
+	</div>
 </div>
-
-<style lang="postcss">
-	.tag {
-		@apply flex items-center overflow-hidden rounded-md [word-break:break-word];
-		@apply bg-magnum-600 text-white;
-		@apply data-[selected]:bg-teal-500;
-		@apply data-[disabled]:bg-magnum-300 data-[disabled]:hover:cursor-default;
-		@apply data-[disabled]:focus:!outline-none data-[disabled]:focus:!ring-0;
-	}
-
-	.tag-delete {
-		@apply flex h-full items-center px-1;
-		@apply enabled:hover:bg-magnum-700;
-		@apply data-[selected]:hover:bg-teal-600;
-	}
-</style>
