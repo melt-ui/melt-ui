4e764e9932b070e462baa6725008e675c58afb96
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-11 11:59:57 +0100

    builder markdown snippet

--- src/lib/builders/popover/types.ts
+++ src/lib/builders/popover/types.ts
@@ -1,10 +1,17 @@
 import type { FloatingConfig } from '$lib/internal/actions';
 import type { createPopover } from './create';
 
+type ChangeFn<T> = (args: { prev: T; next: T }) => boolean;
+
 export type CreatePopoverArgs = {
 	positioning?: FloatingConfig;
 	arrowSize?: number;
+	/** The initial state of open */
 	open?: boolean;
+	/** Optional function that runs whenever open should change.
+	 * When present, will control state changes instead of the default behaviour
+	 * */
+	onOpenChange?: ChangeFn<boolean>;
 };
 
 export type CreatePopoverReturn = ReturnType<typeof createPopover>;
