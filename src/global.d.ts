2f583d16ff670c9b74135441cf2ef18bf2605f57
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-11 13:21:50 -0400

    move the global

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-namespace svelteHTML {
-	interface HTMLAttributes {
-		/**
-		 * TODO: Document me later
-		 */
-		melt?: Record<string, any> & { action: Action<any, any> };
-	}
-}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,24 @@
 export * from './builders';
+
+import type { Action } from 'svelte/action';
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace svelteHTML {
+		interface HTMLAttributes {
+			/**
+			 * TODO: Document me later
+			 *
+			 * @example
+			 * ```svelte
+			 * <script>
+			 * 	const { builder } = createBuilder();
+			 * </script>
+			 *
+			 * <div melt={$builder} />
+			 * ```
+			 */
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			melt?: Record<string, any> & { action: Action<any, any> };
+		}
+	}
+}
