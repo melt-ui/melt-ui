12b02864705042de4ff7b42b669b14fd4f59d0a0
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-14 15:07:57 +0100

    attempt pw ci fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173/',
+		baseURL: process.env.CI ? 'http://localhost:4173/' : 'http://localhost:5173/',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -71,8 +71,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-	  command: 'pnpm run dev',
-	  url: 'http://127.0.0.1:5173',
+	  command: process.env.CI ? 'pnpm run build && pnpm run preview' : 'pnpm run dev',
+	  url: process.env.CI ? 'http://localhost:4173/' : 'http://localhost:5173/',
 	  reuseExistingServer: !process.env.CI,
 		timeout: 30 * 1000,
 	},
