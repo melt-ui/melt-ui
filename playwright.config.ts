71c12760bd0d0f2c32a48eb4aee6103779e0cb1a
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-14 14:45:54 +0100

    attempt pw ci fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-	  command: 'npm run dev',
+	  command: 'pnpm run dev',
 	  url: 'http://127.0.0.1:5173',
 	  reuseExistingServer: !process.env.CI,
-		timeout: 120 * 1000,
+		timeout: 30 * 1000,
 	},
 });
