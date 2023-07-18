135c8e0fb459bb481f833c185d7745fbcb19fca6
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-18 17:05:42 +0100

    fix; lint

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -7,7 +7,7 @@
 <div class="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6">
 	<aside
 		class="fixed top-16 z-30 -ml-2 hidden h-[calc(100vh-4rem)] w-full shrink-0
-		flex-col pb-2 md:sticky md:flex overflow-y-auto"
+		flex-col overflow-y-auto pb-2 md:sticky md:flex"
 	>
 		<div class="py-6 pr-6 lg:py-8">
 			<SidebarNav />
