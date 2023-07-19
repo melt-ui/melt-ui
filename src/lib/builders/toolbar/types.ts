352e3b646621d57441326b14cf06c67f251ab347
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-19 19:32:00 -0400

    export toolbar group sub-builder types

--- src/lib/builders/toolbar/types.ts
+++ src/lib/builders/toolbar/types.ts
@@ -25,3 +25,9 @@ export type Toolbar = BuilderReturn<typeof createToolbar>;
 export type ToolbarElements = Toolbar['elements'];
 export type ToolbarOptions = Toolbar['options'];
 export type ToolbarBuilders = Toolbar['builders'];
+
+export type ToolbarGroup = BuilderReturn<ToolbarBuilders['createToolbarGroup']>;
+export type ToolbarGroupElements = ToolbarGroup['elements'];
+export type ToolbarGroupOptions = ToolbarGroup['options'];
+export type ToolbarGroupStates = ToolbarGroup['states'];
+export type ToolbarGroupHelpers = ToolbarGroup['helpers'];
