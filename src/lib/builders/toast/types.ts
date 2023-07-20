bd689c44001b110bea7a8abf509a1c114382c779
Hunter Johnston <johnstonhuntera@gmail.com>
2023-07-20 15:42:18 -0400

    add toasts return types

--- src/lib/builders/toast/types.ts
+++ src/lib/builders/toast/types.ts
@@ -1,3 +1,4 @@
+import type { BuilderReturn } from '$lib/internal/types';
 import type { createToasts } from './create';
 
 export type EmptyType = Record<never, never>;
@@ -23,4 +24,8 @@ export type Toast<T = object> = {
 	data: T;
 };
 
-export type CreateToastReturn = ReturnType<typeof createToasts>;
+export type Toasts = BuilderReturn<typeof createToasts>;
+export type ToastsElements = Toasts['elements'];
+export type ToastsOptions = Toasts['options'];
+export type ToastsStates = Toasts['states'];
+export type ToastsHelpers = Toasts['helpers'];
