36da43992294e55c0f8170ccdd0e6c925cf9318a
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-24 00:15:59 +0100

    pagination tests

---
+++ src/lib/builders/pagination/tests/cmp.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { createPagination } from '@melt-ui/svelte';
+	import ChevronLeft from '~icons/lucide/chevron-left';
+	import ChevronRight from '~icons/lucide/chevron-right';
+
+	const { prevButton, nextButton, pages, pageTrigger, range, root } = createPagination({
+		count: 100,
+		perPage: 10,
+		page: 1,
+		siblingCount: 1,
+	});
+</script>
+
+<nav aria-label="pagination" melt={$root} data-testid="root">
+	<p>
+		Showing items {$range.start} - {$range.end}
+	</p>
+	<div>
+		<button melt={$prevButton} data-testid="prev"><ChevronLeft /></button>
+		{#each $pages as page (page.key)}
+			{#if page.type === 'ellipsis'}
+				<span>...</span>
+			{:else}
+				<button melt={$pageTrigger(page)}>{page.value}</button>
+			{/if}
+		{/each}
+		<button melt={$nextButton} data-testid="next"><ChevronRight /></button>
+	</div>
+</nav>
