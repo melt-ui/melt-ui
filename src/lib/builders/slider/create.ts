2827de9d11b3def827af99575e41b150b9d4a66a
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-28 00:03:09 +0100

    fix: slider min/max bug

--- src/lib/builders/slider/create.ts
+++ src/lib/builders/slider/create.ts
@@ -250,11 +250,16 @@ export const createSlider = (props: CreateSliderProps = defaults) => {
 			const percent = (clientXY - leftOrBottom) / (rightOrTop - leftOrBottom);
 			const val = Math.round(percent * ($max - $min) + $min);
 
-			if (val < $min || val > $max) return;
-			const step = $options.step;
-			const newValue = Math.round(val / step) * step;
+			if (val < $min) {
+				updatePosition($min, activeThumbIdx);
+			} else if (val > $max) {
+				updatePosition($max, activeThumbIdx);
+			} else {
+				const step = $options.step;
+				const newValue = Math.round(val / step) * step;
 
-			updatePosition(newValue, activeThumbIdx);
+				updatePosition(newValue, activeThumbIdx);
+			}
 		};
 
 		const getClosestThumb = (e: PointerEvent) => {
