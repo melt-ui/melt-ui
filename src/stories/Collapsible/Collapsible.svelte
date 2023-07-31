bc5164b128837e2883ab11456c7156a29075c286
paoloricciuti <ricciutipaolo@gmail.com>
2023-07-31 18:22:32 +0200

    Add eslint rule to prevent imports from lib/builders

--- src/stories/Collapsible/Collapsible.svelte
+++ src/stories/Collapsible/Collapsible.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { createCollapsible } from '$lib/builders/collapsible';
+	import { createCollapsible } from '$lib';
 	import { PreviewWrapper } from '$docs/components';
 	import { slide } from 'svelte/transition';
 	import X from '~icons/lucide/x';
