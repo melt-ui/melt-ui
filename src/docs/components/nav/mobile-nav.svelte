81c3f8c9fb2e8b218552a1890c7018140f96c640
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-01 19:25:53 +0100

    fix moble menu

--- src/docs/components/nav/mobile-nav.svelte
+++ src/docs/components/nav/mobile-nav.svelte
@@ -10,7 +10,7 @@
 
 	const open = writable(false);
 	const {
-		elements: { trigger, overlay, content, close },
+		elements: { trigger, overlay, content, close, portalled },
 	} = createDialog({
 		open,
 	});
@@ -23,65 +23,67 @@
 	<Menu class="square-6" />
 	<span class="sr-only">Toggle Menu</span>
 </button>
-{#if $open}
-	<div
-		use:melt={$overlay}
-		class="fixed inset-0 z-50 bg-black/80 backdrop-blur-sm"
-		transition:fade={{ duration: 150 }}
-	/>
-	<div
-		use:melt={$content}
-		class="menu safe-area fixed bottom-0 z-50 h-2/3 w-full bg-neutral-900 px-2
+<div use:melt={$portalled}>
+	{#if $open}
+		<div
+			use:melt={$overlay}
+			class="fixed inset-0 z-50 bg-black/80 backdrop-blur-sm"
+			transition:fade={{ duration: 150 }}
+		/>
+		<div
+			use:melt={$content}
+			class="menu safe-area fixed bottom-0 z-50 h-2/3 w-full bg-neutral-900 px-2
 				 pt-6 shadow-lg focus:outline-none"
-		transition:fly={{ y: 768, duration: 300, opacity: 1 }}
-	>
-		<div class="flex items-center justify-between">
-			<MobileNavLink href="/" {open}>
-				<img src="/logo.svg" alt="Melt UI" class="h-9" />
-			</MobileNavLink>
-			<Button class="px-2" size="sm" variant="faded" {...$close} action={$close.action}>
-				<X class="square-4" />
-			</Button>
-		</div>
-
-		<div class="mt-2 flex justify-center rounded-lg bg-neutral-800 px-4 py-2">
-			<Switch id="preprocessor" bind:checked={$usingPreprocessor} keepState>
-				<a href="/docs/preprocessor" class="underline"> Preprocessor </a>
-			</Switch>
-		</div>
-
-		<div class="mb-4 mt-1 h-full overflow-auto overflow-x-visible py-2 pr-4">
-			<div class="flex flex-col">
-				{#each navConfig.mainNav as navItem, index (navItem + index.toString())}
-					{#if navItem.href}
-						<MobileNavLink href={navItem.href} {open}>
-							{navItem.title}
-						</MobileNavLink>
-					{/if}
-				{/each}
+			transition:fly={{ y: 768, duration: 300, opacity: 1 }}
+		>
+			<div class="flex items-center justify-between">
+				<MobileNavLink href="/" {open}>
+					<img src="/logo.svg" alt="Melt UI" class="h-9" />
+				</MobileNavLink>
+				<Button class="px-2" size="sm" variant="faded" {...$close} action={$close.action}>
+					<X class="square-4" />
+				</Button>
 			</div>
-			<div class="flex flex-col space-y-2">
-				{#each navConfig.sidebarNav as navItem, index (index)}
-					<div class="flex flex-col pt-6">
-						<span
-							class="rounded-md px-3 pb-2 text-sm font-semibold uppercase tracking-wider text-neutral-400"
-							>{navItem.title}</span
-						>
-						{#if navItem?.items?.length}
-							{#each navItem.items as item}
-								{#if !item.disabled && item.href}
-									<MobileNavLink href={item.href} {open}>
-										{item.title}</MobileNavLink
-									>
-								{/if}
-							{/each}
+
+			<div class="mt-2 flex justify-center rounded-lg bg-neutral-800 px-4 py-2">
+				<Switch id="preprocessor" bind:checked={$usingPreprocessor} keepState>
+					<a href="/docs/preprocessor" class="underline"> Preprocessor </a>
+				</Switch>
+			</div>
+
+			<div class="mb-4 mt-1 h-full overflow-auto overflow-x-visible py-2 pr-4">
+				<div class="flex flex-col">
+					{#each navConfig.mainNav as navItem, index (navItem + index.toString())}
+						{#if navItem.href}
+							<MobileNavLink href={navItem.href} {open}>
+								{navItem.title}
+							</MobileNavLink>
 						{/if}
-					</div>
-				{/each}
+					{/each}
+				</div>
+				<div class="flex flex-col space-y-2">
+					{#each navConfig.sidebarNav as navItem, index (index)}
+						<div class="flex flex-col pt-6">
+							<span
+								class="rounded-md px-3 pb-2 text-sm font-semibold uppercase tracking-wider text-neutral-400"
+								>{navItem.title}</span
+							>
+							{#if navItem?.items?.length}
+								{#each navItem.items as item}
+									{#if !item.disabled && item.href}
+										<MobileNavLink href={item.href} {open}>
+											{item.title}</MobileNavLink
+										>
+									{/if}
+								{/each}
+							{/if}
+						</div>
+					{/each}
+				</div>
 			</div>
 		</div>
-	</div>
-{/if}
+	{/if}
+</div>
 
 <style lang="postcss">
 	.menu,
