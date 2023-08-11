8e1b60eb759512c147a158f5f6ce7b9706de84d5
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-11 16:39:32 +0100

    cleanup tags input test

--- src/tests/tags-input/TagsInput.svelte
+++ src/tests/tags-input/TagsInput.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { createTagsInput, melt, type AddTag } from '$lib/index.js';
-	import { X } from 'lucide-svelte';
 
 	export let defaultTags = ['Svelte', 'Typescript'];
 	export let unique = true;
@@ -20,42 +19,15 @@
 </script>
 
 <main>
-	<div class="flex flex-col items-start justify-center gap-2">
-		<div
-			use:melt={$root}
-			class="flex min-w-[280px] flex-row flex-wrap gap-2.5 rounded-md bg-white px-3 py-2 text-magnum-700
-		focus-within:ring focus-within:ring-magnum-400"
-		>
-			{#each $tags as t, i}
-				<div
-					data-testid="tag-{i}"
-					use:melt={$tag(t)}
-					class="flex items-center overflow-hidden rounded-md bg-magnum-200 text-magnum-900 [word-break:break-word]
-		data-[disabled]:bg-magnum-300 data-[selected]:bg-magnum-400 data-[disabled]:hover:cursor-default
-		data-[disabled]:focus:!outline-none data-[disabled]:focus:!ring-0"
-				>
-					<span class="flex items-center border-r border-white/10 px-1.5">{t.value}</span>
-					<button
-						data-testid="delete-tag-{i}"
-						use:melt={$deleteTrigger(t)}
-						class="flex h-full items-center px-1 enabled:hover:bg-magnum-300"
-					>
-						<X class="square-3" />
-					</button>
-				</div>
-				<div
-					data-testid="edit-tag-{i}"
-					use:melt={$edit(t)}
-					class="flex items-center overflow-hidden rounded-md px-1.5 [word-break:break-word] data-[invalid-edit]:focus:!ring-red-500"
-				/>
-			{/each}
+	<div use:melt={$root}>
+		{#each $tags as t, i}
+			<div data-testid="tag-{i}" use:melt={$tag(t)}>
+				<span>{t.value}</span>
+				<button data-testid="delete-tag-{i}" use:melt={$deleteTrigger(t)}> Delete </button>
+			</div>
+			<div data-testid="edit-tag-{i}" use:melt={$edit(t)} />
+		{/each}
 
-			<input
-				use:melt={$input}
-				type="text"
-				placeholder="Enter tags..."
-				class="min-w-[4.5rem] shrink grow basis-0 border-0 text-black outline-none focus:!ring-0 data-[invalid]:text-red-500"
-			/>
-		</div>
+		<input use:melt={$input} type="text" placeholder="Enter tags..." />
 	</div>
 </main>
