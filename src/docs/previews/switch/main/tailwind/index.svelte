e2dca70948f55c2a7a2e34742d2ac027bcc723bf
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-17 22:39:21 +0100

    ensure switch example is accessible

--- src/docs/previews/switch/main/tailwind/index.svelte
+++ src/docs/previews/switch/main/tailwind/index.svelte
@@ -9,13 +9,18 @@
 
 <form>
 	<div class="flex items-center">
-		<label class="pr-4 leading-none text-white" for="airplane-mode">
+		<label
+			class="pr-4 leading-none text-white"
+			for="airplane-mode"
+			id="airplane-mode-label"
+		>
 			Airplane mode
 		</label>
 		<button
 			use:melt={$root}
 			class="relative h-6 w-11 cursor-default rounded-full bg-magnum-800 transition-colors data-[state=checked]:bg-magnum-950"
 			id="airplane-mode"
+			aria-labelledby="airplane-mode-label"
 		>
 			<span
 				class="block h-5 w-5 translate-x-0.5 rounded-full bg-white
