04cf8f7fec51c2b00201295082caa1ba1360da1d
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-18 17:39:40 +0100

    remove debug preview

--- src/docs/previews/pin-input/main/tailwind/index.svelte
+++ src/docs/previews/pin-input/main/tailwind/index.svelte
@@ -3,12 +3,9 @@
 
 	const {
 		elements: { root, input },
-		states: { value },
 	} = createPinInput();
 </script>
 
-<button on:click={() => value.set('12345'.split(''))}> Set to 12345 </button>
-{$value}
 <div use:melt={$root} class="flex items-center gap-2">
 	{#each Array.from({ length: 5 }) as _, i}
 		<input
