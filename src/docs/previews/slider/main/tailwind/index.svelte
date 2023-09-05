abee8e549204c795551632b76671de04d24d7eb5
tglide <26071571+TGlide@users.noreply.github.com>
2023-09-05 20:28:43 +0100

    fix slider example

--- src/docs/previews/slider/main/tailwind/index.svelte
+++ src/docs/previews/slider/main/tailwind/index.svelte
@@ -3,22 +3,13 @@
 
 	const {
 		elements: { root, range, thumb },
-		states: { value },
 	} = createSlider({
-		defaultValue: [0],
-		max: 13,
-		step: 5,
-		onValueChange({ curr, next }) {
-			const isMultipleOf5 = next[0] % 5 === 0;
-			if (!isMultipleOf5) {
-				return curr;
-			}
-			return next;
-		},
+		defaultValue: [30],
+		max: 100,
+		step: 1,
 	});
 </script>
 
-{$value}
 <span use:melt={$root} class="relative flex h-[20px] w-[200px] items-center">
 	<span class="block h-[3px] w-full bg-black/40">
 		<span use:melt={$range} class="h-[3px] bg-white" />
