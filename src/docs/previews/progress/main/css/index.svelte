9d4ee81d4030db309d3b14a1cdba36f51eb8fb78
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-10-03 19:57:37 +0000

    docs: css preview for Progress

---
+++ src/docs/previews/progress/main/css/index.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { createProgress, melt } from '$lib/index.js';
+	import { writable } from 'svelte/store';
+
+	const value = writable(30);
+
+	const {
+		elements: { root },
+		options: { max },
+	} = createProgress({
+		value,
+		max: 100,
+	});
+
+	const sleep = (ms: number) =>
+		new Promise((resolve) => setTimeout(resolve, ms));
+	sleep(1000).then(() => {
+		value.set(75);
+	});
+</script>
+
+<div use:melt={$root} class="track">
+	<div
+		class="progress-bar"
+		style={`transform: translateX(-${
+			100 - (100 * ($value ?? 0)) / ($max ?? 1)
+		}%)`}
+	/>
+</div>
+
+<style>
+	.track {
+		position: relative;
+		height: 24px;
+		width: 300px;
+		overflow: hidden;
+		border-radius: 99999px;
+		background-color: rgb(0 0 0 / 0.4);
+	}
+
+	.progress-bar {
+		width: 100%;
+		height: 100%;
+		background-color: white;
+    transition: transform 660ms cubic-bezier(0.65, 0, 0.35, 1);
+	}
+</style>
