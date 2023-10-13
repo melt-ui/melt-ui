ad9a93918e48879b0a836db490d6c5ef67ada57b
Bakemono-san <mousdef34@gmail.com>
2023-10-13 14:55:52 +0000

    Docs:Preview-css tree

---
+++ src/docs/previews/tree/main/css/index.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+	import { createTreeView } from '$lib/index.js';
+	import { setContext } from 'svelte';
+
+	import type { TreeItem } from './tree.svelte';
+	import Tree from './tree.svelte';
+
+	const ctx = createTreeView({
+		defaultExpanded: ['lib-0', 'tree-0'],
+	});
+	setContext('tree', ctx);
+
+	const {
+		elements: { tree },
+	} = ctx;
+
+	const treeItems: TreeItem[] = [
+		{ title: 'index.svelte', icon: 'svelte' },
+		{
+			title: 'lib',
+			icon: 'folder',
+			children: [
+				{
+					title: 'tree',
+					icon: 'folder',
+					children: [
+						{
+							title: 'Tree.svelte',
+							icon: 'svelte',
+						},
+						{
+							title: 'TreeView.svelte',
+							icon: 'svelte',
+						},
+					],
+				},
+				{
+					title: 'icons',
+					icon: 'folder',
+					children: [
+						{ title: 'JS.svelte', icon: 'svelte' },
+						{ title: 'Svelte.svelte', icon: 'svelte' },
+					],
+				},
+				{
+					title: 'index.js',
+					icon: 'js',
+				},
+			],
+		},
+		{
+			title: 'routes',
+			icon: 'folder',
+			children: [
+				{
+					title: 'contents',
+					icon: 'folder',
+					children: [
+						{
+							title: '+layout.svelte',
+							icon: 'svelte',
+						},
+						{
+							title: '+page.svelte',
+							icon: 'svelte',
+						},
+					],
+				},
+			],
+		},
+	];
+</script>
+
+<div class="container">
+	<div class="main">
+		<h3 class="main-title">Project Structure</h3>
+		<hr />
+	</div>
+
+	<ul class="tree" {...$tree}>
+		<Tree {treeItems} />
+	</ul>
+</div>
+
+<style>
+	* {
+		all: unset;
+	}
+
+	:root {
+		--neutral-900: rgb(23, 23, 23, 1);
+	}
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		padding: 1rem 1rem 0rem 1rem;
+	}
+
+	.main-title {
+		font-weight: 700;
+		font-size: 1.125rem;
+		line-height: 1.125rem;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		width: 18.75rem;
+		height: 18.75rem;
+		background-color: white;
+		color: var(--neutral-900);
+		border-radius: 0.75rem;
+	}
+
+	.tree {
+		overflow: auto;
+		padding: 0.5rem 1rem 1rem 1rem;
+	}
+
+	@media (min-width: 768px) {
+		.container {
+			height: 350px;
+		}
+	}
+</style>
---
+++ src/docs/previews/tree/main/css/tree.svelte
@@ -0,0 +1,109 @@
+<script context="module" lang="ts">
+	import { ArrowLeft, Folder, FolderOpen } from 'lucide-svelte';
+	import JS from './icons/JS.svelte';
+	import Svelte from './icons/Svelte.svelte';
+
+	type Icon = 'svelte' | 'folder' | 'js';
+
+	export type TreeItem = {
+		title: string;
+		icon: Icon;
+
+		children?: TreeItem[];
+	};
+
+	export const icons = {
+		svelte: Svelte,
+		folder: Folder,
+		folderOpen: FolderOpen,
+		js: JS,
+		highlight: ArrowLeft,
+	};
+</script>
+
+<script lang="ts">
+	import { melt, type TreeView } from '$lib/index.js';
+	import { getContext } from 'svelte';
+
+	export let treeItems: TreeItem[];
+	export let level = 1;
+
+	const {
+		elements: { item, group },
+		helpers: { isExpanded, isSelected },
+	} = getContext<TreeView>('tree');
+</script>
+
+{#each treeItems as { title, icon, children }, i}
+	{@const itemId = `${title}-${i}`}
+	{@const hasChildren = !!children?.length}
+
+	<li class={level !== 1 ? 'itemContainer' : ''}>
+		<button
+			class="item"
+			use:melt={$item({
+				id: itemId,
+				hasChildren,
+			})}
+		>
+			<!-- Add icon. -->
+			{#if icon === 'folder' && hasChildren && $isExpanded(itemId)}
+				<svelte:component this={icons['folderOpen']} class="square-4" />
+			{:else}
+				<svelte:component this={icons[icon]} class="square-4" />
+			{/if}
+
+			<span class="select-none">{title}</span>
+
+			<!-- Selected icon. -->
+			{#if $isSelected(itemId)}
+				<svelte:component this={icons['highlight']} class="square-4" />
+			{/if}
+		</button>
+
+		{#if children}
+			<ul use:melt={$group({ id: itemId })}>
+				<svelte:self treeItems={children} level={level + 1} />
+			</ul>
+		{/if}
+	</li>
+{/each}
+
+<style>
+	:root {
+		--magnum-200: #fce0ac;
+	}
+
+	* {
+		all: unset;
+	}
+
+	/* Remove docs' focus box-shadow styling. */
+	li:focus {
+		box-shadow: none !important;
+	}
+
+	.itemContainer {
+		padding-left: 1rem;
+	}
+
+	.select-none {
+		user-select: none;
+	}
+
+	.square-4 {
+		width: 1rem;
+		height: 1rem;
+	}
+
+	.item {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+		padding: 0.25rem;
+		border-radius: 0.375rem;
+	}
+	.item:focus {
+		background-color: var(--magnum-200);
+	}
+</style>
