a78e36516f74552b348a2ad331c56f006633413c
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-10-21 18:46:24 -0700

    feat: add devcontainer configuration

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# when in a VS Code or GitHub Codespaces devcontainer
+if [ -n "${REMOTE_CONTAINERS}" ]; then
+	this_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)
+	workspace_root=$(realpath ${this_dir}/..)
+
+	# perform additional one-time setup just after
+	# the devcontainer is created
+	pnpm install --dir "${workspace_root}" # install workspace node dependencies
+	npx --yes playwright install           # install playwright browsers
+
+fi
