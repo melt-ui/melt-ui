e96108cfe0ecb5689f1a670a57db40e7ef10adfc
Hunter Johnston <johnstonhuntera@gmail.com>
2023-10-25 11:44:27 -0400

    cleanup handleFocus

--- src/lib/internal/helpers/focus.ts
+++ src/lib/internal/helpers/focus.ts
@@ -1,9 +1,4 @@
-import {
-	handleRovingFocus,
-	isFunction,
-	isHTMLElement,
-	sleep,
-} from '$lib/internal/helpers/index.js';
+import { isFunction, isHTMLElement, sleep } from '$lib/internal/helpers/index.js';
 
 export type FocusTarget = string | HTMLElement | SVGElement | null;
 export type FocusProp = FocusTarget | ((defaultEl?: HTMLElement | null) => FocusTarget);
@@ -11,26 +6,12 @@ export type FocusProp = FocusTarget | ((defaultEl?: HTMLElement | null) => Focus
 type HandleFocusArgs = {
 	prop?: FocusProp;
 	defaultEl: HTMLElement | null;
-	roving?: boolean;
 };
 
 export function handleFocus(args: HandleFocusArgs): void {
-	const { prop, defaultEl, roving } = args;
+	const { prop, defaultEl } = args;
 	if (prop === undefined) {
-		if (roving && defaultEl) {
-			/**
-			 * Roving focus modifies the tab index of the currently focused element
-			 * regardless of who _owns_ that element. We don't want to modify the tab
-			 * index of something we do not control/own, so we only do this if the
-			 * defaultEl is provided.
-			 *
-			 * This is primarily used within the Menu builders to manage focus between
-			 * menu items.
-			 */
-			handleRovingFocus(defaultEl);
-		} else {
-			sleep(1).then(() => defaultEl?.focus());
-		}
+		sleep(1).then(() => defaultEl?.focus());
 		return;
 	}
 
