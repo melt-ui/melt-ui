2fa1ba1e0ba2729342daa0bdab0b0a0bf2b7be68
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-30 13:53:11 +0000

    setup uno

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import UnoCSS from '@unocss/svelte-scoped/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+			injectReset: `@unocss/reset/tailwind.css`, // see type definition for all included reset options or how to pass in your own
+			// ...other Svelte Scoped options
+		}),
+		sveltekit(),
+	],
 	test: {
 		include: ['src/**/*.spec.{js,ts}'],
 		// jest like globals
