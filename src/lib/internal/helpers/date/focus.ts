2733f190c34b64f57eeff75350c088b8c30485ed
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-05 10:57:20 -0500

    fix autofocus

--- src/lib/internal/helpers/date/focus.ts
+++ src/lib/internal/helpers/date/focus.ts
@@ -1,8 +1,5 @@
 import { isHTMLElement } from '$lib/internal/helpers/index.js';
 
-export function pickerOpenFocus(defaultEl?: HTMLElement | null) {
-	const focusedCell = document.querySelector('[data-melt-calendar-cell][data-focused]');
-	if (isHTMLElement(focusedCell)) return focusedCell;
-	if (defaultEl) return defaultEl;
-	return null;
+export function pickerOpenFocus(_?: HTMLElement | null) {
+	return document.querySelector('[data-melt-calendar-cell][data-focused]');
 }
--- src/lib/internal/helpers/focus.ts
+++ src/lib/internal/helpers/focus.ts
@@ -10,20 +10,24 @@ type HandleFocusArgs = {
 
 export function handleFocus(args: HandleFocusArgs): void {
 	const { prop, defaultEl } = args;
-	if (prop === undefined) {
-		sleep(1).then(() => defaultEl?.focus());
-		return;
-	}
 
-	const returned = isFunction(prop) ? prop(defaultEl) : prop;
+	sleep(1).then(() => {
+		if (prop === undefined) {
+			defaultEl?.focus();
+			return;
+		}
 
-	if (typeof returned === 'string') {
-		// Get el by selector, focus it
-		const el = document.querySelector(returned);
-		if (!isHTMLElement(el)) return;
-		sleep(1).then(() => el.focus());
-	} else if (isHTMLElement(returned)) {
-		// Focus it
-		sleep(1).then(() => returned.focus());
-	}
+		const returned = isFunction(prop) ? prop(defaultEl) : prop;
+		if (typeof returned === 'string') {
+			// Get el by selector, focus it
+			sleep(1).then(() => {
+				const el = document.querySelector(returned);
+				if (!isHTMLElement(el)) return;
+				el.focus();
+			});
+		} else if (isHTMLElement(returned)) {
+			// Focus it
+			sleep(1).then(() => returned.focus());
+		}
+	});
 }
