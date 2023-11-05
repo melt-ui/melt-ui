3e2617da252768047eb6f49aa4b96cb8bcf5e465
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-05 11:14:54 -0500

    fix openfocus handler

--- src/lib/internal/helpers/date/focus.ts
+++ src/lib/internal/helpers/date/focus.ts
@@ -1,5 +1,12 @@
 import { isHTMLElement } from '$lib/internal/helpers/index.js';
 
-export function pickerOpenFocus(_?: HTMLElement | null) {
-	return document.querySelector('[data-melt-calendar-cell][data-focused]');
+export function pickerOpenFocus(defaultEl?: HTMLElement | null) {
+	const el = document.querySelector('[data-melt-calendar-cell][data-focused]');
+	if (isHTMLElement(el)) {
+		return el;
+	}
+	if (isHTMLElement(defaultEl)) {
+		return defaultEl;
+	}
+	return null;
 }
