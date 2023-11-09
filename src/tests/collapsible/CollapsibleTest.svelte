821d1434af8c4ecf27d4885d75bd87f0a2bc20c5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-08 20:47:36 -0500

    Update tests

--- src/tests/collapsible/CollapsibleTest.svelte
+++ src/tests/collapsible/CollapsibleTest.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-	import { createCollapsible, melt } from '$lib/index.js';
+	import { createCollapsible, melt, type CreateCollapsibleProps } from '$lib/index.js';
 
-	export let open = false;
+	export let defaultOpen: CreateCollapsibleProps['defaultOpen'] = undefined;
+	export let open: CreateCollapsibleProps['open'] = undefined;
+	export let disabled: CreateCollapsibleProps['disabled'] = undefined;
+	export let onOpenChange: CreateCollapsibleProps['onOpenChange'] = undefined;
+	export let forceVisible: CreateCollapsibleProps['forceVisible'] = undefined;
 
 	const {
 		elements: { root, trigger, content },
 	} = createCollapsible({
-		defaultOpen: open,
+		defaultOpen,
+		disabled,
+		onOpenChange,
+		forceVisible,
+		open,
 	});
 </script>
 
