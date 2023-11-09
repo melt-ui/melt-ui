aa2724b5d841cf462465b689d2d2a44e7cffccb5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-08 20:50:44 -0500

    remove unnecessary props

--- src/tests/collapsible/CollapsibleTest.svelte
+++ src/tests/collapsible/CollapsibleTest.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
 	import { createCollapsible, melt, type CreateCollapsibleProps } from '$lib/index.js';
 
-	export let defaultOpen: CreateCollapsibleProps['defaultOpen'] = undefined;
-	export let open: CreateCollapsibleProps['open'] = undefined;
-	export let disabled: CreateCollapsibleProps['disabled'] = undefined;
-	export let onOpenChange: CreateCollapsibleProps['onOpenChange'] = undefined;
-	export let forceVisible: CreateCollapsibleProps['forceVisible'] = undefined;
+	type $$Props = CreateCollapsibleProps;
 
 	const {
 		elements: { root, trigger, content },
 	} = createCollapsible({
-		defaultOpen,
-		disabled,
-		onOpenChange,
-		forceVisible,
-		open,
+		...$$restProps,
 	});
 </script>
 
