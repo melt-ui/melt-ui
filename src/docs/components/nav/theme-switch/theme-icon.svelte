1e99142a60a23ec377c7b605433b8a7e1d8b4e81
Syméon Smith <symeon.smith@gmail.com>
2023-11-14 23:11:35 +0100

    feat: use svelte:component to render theme icons

--- src/docs/components/nav/theme-switch/theme-icon.svelte
+++ src/docs/components/nav/theme-switch/theme-icon.svelte
@@ -2,15 +2,9 @@
 	import { Monitor, Moon, Sun } from 'lucide-svelte';
 	import type { Theme } from './types';
 
-	export let theme: Theme = 'dark';
+	export let theme: Theme;
 
-	const classes = 'h-5 w-5';
+	$: component = theme === 'dark' ? Moon : theme === 'light' ? Sun : Monitor;
 </script>
 
-{#if theme === 'dark'}
-	<Moon class={classes} />
-{:else if theme === 'light'}
-	<Sun class={classes} />
-{:else}
-	<Monitor class={classes} />
-{/if}
+<svelte:component this={component} class="h-5 w-5" />
