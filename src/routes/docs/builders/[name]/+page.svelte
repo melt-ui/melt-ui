283db45d529d1e586b22b2af56452175bb81406e
lolcabanon <90099349+lolcabanon@users.noreply.github.com>
2023-11-15 15:22:18 +0100

    Changed overflow-visible to x-visible y-auto
    To prevent TOC content (Calendar builder and maybe more?) from overflowing at the bottom of the screen and become unreachable. (1920x1080px screen)

--- src/routes/docs/builders/[name]/+page.svelte
+++ src/routes/docs/builders/[name]/+page.svelte
@@ -49,7 +49,7 @@
 		<!-- <DocsPager /> -->
 	</div>
 	<div class="hidden text-sm xl:block">
-		<div class="fixed top-16 h-[calc(100vh-4rem)] overflow-visible pt-6">
+		<div class="fixed top-16 h-[calc(100vh-4rem)] overflow-x-visible overflow-y-auto pt-6">
 			{#key $page.url.pathname}
 				<TOC />
 			{/key}
