1a6a0233f4a963098cbbcaa450e02d5467b682ef
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-27 16:08:22 -0500

    fix: datepicker popover reactivity

--- src/lib/builders/date-picker/create.ts
+++ src/lib/builders/date-picker/create.ts
@@ -121,6 +121,26 @@ export function createDatePicker(props?: CreateDatePickerProps) {
 		calendar.options.maxValue.set($maxValue);
 	});
 
+	effect([options.positioning], ([$positioning]) => {
+		popover.options.positioning.set($positioning);
+	});
+
+	effect([options.forceVisible], ([$forceVisible]) => {
+		popover.options.forceVisible.set($forceVisible);
+	});
+
+	effect([options.closeOnEscape], ([$closeOnEscape]) => {
+		popover.options.closeOnEscape.set($closeOnEscape);
+	});
+
+	effect([options.closeOnOutsideClick], ([$closeOnOutsideClick]) => {
+		popover.options.closeOnOutsideClick.set($closeOnOutsideClick);
+	});
+
+	effect([options.preventScroll], ([$preventScroll]) => {
+		popover.options.preventScroll.set($preventScroll);
+	});
+
 	const dateFieldOptions = omit(
 		dateField.options,
 		'locale',
