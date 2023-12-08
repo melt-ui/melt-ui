c0465c536373d77a4c22500bc651011356ed78ce
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-12-08 19:03:57 +0100

    add tests for pin input

---
+++ src/tests/pin-input/PinInputTest.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createPinInput, melt, type CreatePinInputProps } from '$lib/index.js';
+
+	export let placeholder: CreatePinInputProps['placeholder'] = '○';
+	export let name: CreatePinInputProps['name'] = undefined;
+	export let disabled: CreatePinInputProps['disabled'] = false;
+	export let type: CreatePinInputProps['type'] = 'text';
+	export let defaultValue: CreatePinInputProps['defaultValue'] = ['1', '2', '3', '4', '5'];
+	export let value: CreatePinInputProps['value'] = undefined;
+	export let onValueChange: CreatePinInputProps['onValueChange'] = undefined;
+
+	const {
+		elements: { root, input, hiddenInput },
+	} = createPinInput({
+		placeholder,
+		name,
+		disabled,
+		type,
+		defaultValue,
+		value,
+		onValueChange,
+	});
+</script>
+
+<main>
+	<div use:melt={$root} data-testid="root">
+		{#each Array(5) as _, i}
+			<input use:melt={$input()} data-testid="input-{i + 1}" />
+		{/each}
+		<input use:melt={$hiddenInput} data-testid="hidden-input" />
+	</div>
+</main>
