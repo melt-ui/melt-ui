29cafa67d0ab2f278bf727c8612df1e669403c99
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-26 19:36:14 +0000

    logs in prod yayp

--- src/routes/(landing-ui)/search.svelte
+++ src/routes/(landing-ui)/search.svelte
@@ -26,6 +26,7 @@
 			throw new Error('Failed to load pagefind');
 		}
 		const text = await res.text();
+		console.log('text', text);
 		const blob = new Blob([text], { type: 'application/javascript' });
 		const url = URL.createObjectURL(blob);
 
