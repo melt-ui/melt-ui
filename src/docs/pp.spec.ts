88a9ee35664b610f0c6858520948b2847441cb03
Abdelrahman <abdelrahmanb015@gmail.com>
2024-01-11 11:50:36 +0200

    Slider: Refactor Thumb and Tick (#849)
    Co-authored-by: Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>

--- src/docs/pp.spec.ts
+++ src/docs/pp.spec.ts
@@ -2,15 +2,29 @@ import { test, expect } from 'vitest';
 import { extractMeltAttribute, processMeltAttributes } from './pp.js';
 
 const extractMeltAttributeCases = [
-	{ input: 'use:melt={$trigger(id)}', expected: { builder: 'trigger', args: 'id' } },
-	{ input: 'use:melt={$trigger}', expected: { builder: 'trigger', args: null } },
+	{
+		input: 'use:melt={$trigger(id)}',
+		expected: { builder: '$trigger', args: 'id' },
+	},
+	{
+		input: 'use:melt={$trigger}',
+		expected: { builder: '$trigger' },
+	},
 	{
 		input: "use:melt={$trigger({helpme: 'god'} )}",
-		expected: { builder: 'trigger', args: "{helpme: 'god'} " },
+		expected: { builder: '$trigger', args: "{helpme: 'god'} " },
 	},
 	{
 		input: "use:melt={$trigger({helpme: 'god'} )} awesome={coolio}",
-		expected: { builder: 'trigger', args: "{helpme: 'god'} " },
+		expected: { builder: '$trigger', args: "{helpme: 'god'} " },
+	},
+	{
+		input: 'use:melt={$thumbs[0]}',
+		expected: { builder: '$thumbs', index: '0' },
+	},
+	{
+		input: 'use:melt={thumb}',
+		expected: { builder: 'thumb' },
 	},
 ];
 
@@ -33,6 +47,14 @@ const processMeltAttributesCases = [
 	{
 		input: "use:melt={$trigger({helpme: 'god'} )} awesome={coolio}",
 		expected: "{...$trigger({helpme: 'god'} )} use:trigger awesome={coolio}",
+	},
+	{
+		input: 'use:melt={$thumbs[0]}',
+		expected: '{...$thumbs[0]} use:thumbs',
+	},
+	{
+		input: 'use:melt={thumb}',
+		expected: '{...thumb} use:thumb.action',
 	},
 ];
 
