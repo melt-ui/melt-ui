9d9c571ae6fa656ecf3a4fcbd318f5d994fc0505
tglide <26071571+TGlide@users.noreply.github.com>
2024-01-20 16:20:15 +0000

    before mad genius

--- src/lib/internal/helpers/withGet.ts
+++ src/lib/internal/helpers/withGet.ts
@@ -1,13 +1,13 @@
 import {
-	derived,
 	get,
+	readable,
 	writable,
 	type Readable,
 	type Stores,
 	type StoresValues,
 	type Writable,
 } from 'svelte/store';
-import { effect, isWritable } from '.';
+import { effect, isWritable, noop } from '.';
 
 type ReadableValue<T> = T extends Readable<infer V> ? V : never;
 
@@ -54,21 +54,73 @@ withGet.writable = function <T>(value: T): WithGet<Writable<T>> {
 	return withGet(writable(value));
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function subscribe(store: any, ...callbacks: any[]) {
+	if (store == null) {
+		for (const callback of callbacks) {
+			callback(undefined);
+		}
+		return noop;
+	}
+	const unsub = store.subscribe(...callbacks);
+	return unsub.unsubscribe ? () => unsub.unsubscribe() : unsub;
+}
+
 withGet.derived = function <S extends Stores, T>(
 	stores: S,
 	fn: (values: StoresValues<S>) => T
 ): WithGet<Readable<T>> {
-	let value: ReadableValue<Readable<T>>;
-	const store = derived(stores, (deps) => {
-		const nv = fn(deps);
-		value = nv;
-		return nv;
+	let value: T | undefined = undefined;
+	const single = !Array.isArray(stores);
+	/** @type {Array<import('./public.js').Readable<any>>} */
+	const stores_array = (single ? [stores] : stores) as unknown[];
+	if (!stores_array.every(Boolean)) {
+		throw new Error('derived() expects stores as input, got a falsy value');
+	}
+	const auto = fn.length < 2;
+	const store = readable(value as T | undefined, (set) => {
+		let started = false;
+		const values = [] as StoresValues<S>;
+		let pending = 0;
+		const sync = () => {
+			if (pending) {
+				return;
+			}
+			const result = fn(single ? values[0] : values);
+			if (auto) {
+				set(result);
+				value = result;
+			}
+		};
+		const unsubscribers = stores_array.map((store, i) =>
+			subscribe(
+				store,
+				(value: unknown) => {
+					values[i] = value;
+					pending &= ~(1 << i);
+					if (started) {
+						sync();
+					}
+				},
+				() => {
+					pending |= 1 << i;
+				}
+			)
+		);
+		started = true;
+		sync();
+		return function stop() {
+			unsubscribers.forEach((fn) => fn());
+			// We need to set this to false because callbacks can still happen despite having unsubscribed:
+			// Callbacks might already be placed in the queue which doesn't know it should no longer
+			// invoke this derived store.
+			started = false;
+		};
 	});
 
-	value = get(store);
 	return {
 		...store,
-		get: () => value,
+		get: () => value as T,
 	};
 };
 
