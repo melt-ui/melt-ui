618ee2dfd435cd758f3ffb889e7f1e7e245172d4
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-04 21:30:13 -0500

    remove svelte-action-balancer in favor of text-balance

--- src/docs/components/description.svelte
+++ src/docs/components/description.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { cn } from '$docs/utils/index.js';
-	import { balancer } from 'svelte-action-balancer';
 	let className: string | undefined | null = undefined;
 	export { className as class };
 </script>
 
-<p class={cn('mb-4 mt-2 text-xl text-neutral-400', className)} use:balancer>
+<p class={cn('text-balance mb-4 mt-2 text-xl text-neutral-400', className)}>
 	<slot />
 </p>
