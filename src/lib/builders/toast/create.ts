2adb87948e3d38e15cf5ab43df8cdbcba52a6d73
lolcabanon <julesses@gmail.com>
2024-02-21 23:56:34 +0100

    Added prop to createToaster() to pause all timers on hover

--- src/lib/builders/toast/create.ts
+++ src/lib/builders/toast/create.ts
@@ -1,14 +1,14 @@
 import { usePortal } from '$lib/internal/actions/index.js';
 import {
-	addMeltEventListener,
-	makeElement,
-	createElHelpers,
-	executeCallbacks,
-	generateId,
-	isTouch,
-	kbd,
-	noop,
-	toWritableStores,
+  addMeltEventListener,
+  makeElement,
+  createElHelpers,
+  executeCallbacks,
+  generateId,
+  isTouch,
+  kbd,
+  noop,
+  toWritableStores
 } from '$lib/internal/helpers/index.js';
 import type { MeltActionReturn } from '$lib/internal/types.js';
 import { derived, readonly, writable } from 'svelte/store';
@@ -19,228 +19,253 @@ type ToastParts = 'content' | 'title' | 'description' | 'close';
 const { name } = createElHelpers<ToastParts>('toast');
 
 const defaults = {
-	closeDelay: 5000,
-	type: 'foreground',
+  closeDelay: 5000,
+  type: 'foreground',
+  pauseAllOnHover: false
 } satisfies CreateToasterProps;
 
 export function createToaster<T = object>(props?: CreateToasterProps) {
-	const withDefaults = { ...defaults, ...props } satisfies CreateToasterProps;
+  const withDefaults = { ...defaults, ...props } satisfies CreateToasterProps;
 
-	const options = toWritableStores(withDefaults);
-	const { closeDelay, type } = options;
+  const options = toWritableStores(withDefaults);
+  const { closeDelay, type, pauseAllOnHover } = options;
 
-	const toastsMap = writable(new Map<string, Toast<T>>());
+  const toastsMap = writable(new Map<string, Toast<T>>());
 
-	const addToast = (props: AddToastProps<T>) => {
-		const propsWithDefaults = {
-			closeDelay: closeDelay.get(),
-			type: type.get(),
-			...props,
-		} satisfies AddToastProps<T>;
+  const addToast = (props: AddToastProps<T>) => {
+    const propsWithDefaults = {
+      closeDelay: closeDelay.get(),
+      type: type.get(),
+      ...props
+    } satisfies AddToastProps<T>;
 
-		const ids = {
-			content: generateId(),
-			title: generateId(),
-			description: generateId(),
-		};
+    const ids = {
+      content: generateId(),
+      title: generateId(),
+      description: generateId()
+    };
 
-		const timeout =
-			propsWithDefaults.closeDelay === 0
-				? null
-				: window.setTimeout(() => {
-						removeToast(ids.content);
-				  }, propsWithDefaults.closeDelay);
+    const timeout =
+      propsWithDefaults.closeDelay === 0
+        ? null
+        : window.setTimeout(() => {
+            removeToast(ids.content);
+          }, propsWithDefaults.closeDelay);
 
-		const getPercentage = () => {
-			const { createdAt, pauseDuration, closeDelay, pausedAt } = toast;
-			if (closeDelay === 0) return 0;
+    const getPercentage = () => {
+      const { createdAt, pauseDuration, closeDelay, pausedAt } = toast;
+      if (closeDelay === 0) return 0;
 
-			if (pausedAt) {
-				return (100 * (pausedAt - createdAt - pauseDuration)) / closeDelay;
-			} else {
-				const now = performance.now();
-				return (100 * (now - createdAt - pauseDuration)) / closeDelay;
-			}
-		};
+      if (pausedAt) {
+        return (100 * (pausedAt - createdAt - pauseDuration)) / closeDelay;
+      } else {
+        const now = performance.now();
+        return (100 * (now - createdAt - pauseDuration)) / closeDelay;
+      }
+    };
 
-		const toast = {
-			id: ids.content,
-			ids,
-			...propsWithDefaults,
-			timeout,
-			createdAt: performance.now(),
-			pauseDuration: 0,
-			getPercentage,
-		} as Toast<T>;
+    const toast = {
+      id: ids.content,
+      ids,
+      ...propsWithDefaults,
+      timeout,
+      createdAt: performance.now(),
+      pauseDuration: 0,
+      getPercentage
+    } as Toast<T>;
 
-		toastsMap.update((currentMap) => {
-			currentMap.set(ids.content, toast);
-			return new Map(currentMap);
-		});
+    toastsMap.update((currentMap) => {
+      currentMap.set(ids.content, toast);
+      return new Map(currentMap);
+    });
 
-		return toast;
-	};
+    return toast;
+  };
 
-	const removeToast = (id: string) => {
-		toastsMap.update((currentMap) => {
-			currentMap.delete(id);
-			return new Map(currentMap);
-		});
-	};
+  const removeToast = (id: string) => {
+    toastsMap.update((currentMap) => {
+      currentMap.delete(id);
+      return new Map(currentMap);
+    });
+  };
 
-	const updateToast = (id: string, data: T) => {
-		toastsMap.update((currentMap) => {
-			const toast = currentMap.get(id);
-			if (!toast) return currentMap;
+  const updateToast = (id: string, data: T) => {
+    toastsMap.update((currentMap) => {
+      const toast = currentMap.get(id);
+      if (!toast) return currentMap;
 
-			currentMap.set(id, { ...toast, data });
-			return new Map(currentMap);
-		});
-	};
+      currentMap.set(id, { ...toast, data });
+      return new Map(currentMap);
+    });
+  };
 
-	const content = makeElement(name('content'), {
-		stores: toastsMap,
-		returned: ($toasts) => {
-			return (id: string) => {
-				const t = $toasts.get(id);
-				if (!t) return null;
-				const { ...toast } = t;
+  const pauseToastTimer = (currentToast: Toast<T>) => {
+    if (currentToast.closeDelay !== 0 && currentToast.timeout !== null) {
+      window.clearTimeout(currentToast.timeout);
+    }
+    currentToast.pausedAt = performance.now();
+  };
+  const restartToastTimer = (currentToast: Toast<T>) => {
+    const pausedAt = currentToast.pausedAt ?? currentToast.createdAt;
+    const elapsed =
+      pausedAt - currentToast.createdAt - currentToast.pauseDuration;
+    const remaining = currentToast.closeDelay - elapsed;
+    currentToast.timeout = window.setTimeout(() => {
+      removeToast(currentToast.id);
+    }, remaining);
+    currentToast.pauseDuration += performance.now() - pausedAt;
+    currentToast.pausedAt = undefined;
+  };
 
-				return {
-					id,
-					role: 'alert',
-					'aria-describedby': toast.ids.description,
-					'aria-labelledby': toast.ids.title,
-					'aria-live': toast.type === 'foreground' ? ('assertive' as const) : ('polite' as const),
-					tabindex: -1,
-				};
-			};
-		},
-		action: (node: HTMLElement): MeltActionReturn<ToastEvents['content']> => {
-			let destroy = noop;
+  const content = makeElement(name('content'), {
+    stores: toastsMap,
+    returned: ($toasts) => {
+      return (id: string) => {
+        const t = $toasts.get(id);
+        if (!t) return null;
+        const { ...toast } = t;
 
-			destroy = executeCallbacks(
-				addMeltEventListener(node, 'pointerenter', (e) => {
-					if (isTouch(e)) return;
-					toastsMap.update((currentMap) => {
-						const currentToast = currentMap.get(node.id);
-						if (!currentToast || currentToast.closeDelay === 0) return currentMap;
+        return {
+          id,
+          role: 'alert',
+          'aria-describedby': toast.ids.description,
+          'aria-labelledby': toast.ids.title,
+          'aria-live':
+            toast.type === 'foreground'
+              ? ('assertive' as const)
+              : ('polite' as const),
+          tabindex: -1
+        };
+      };
+    },
+    action: (node: HTMLElement): MeltActionReturn<ToastEvents['content']> => {
+      let destroy = noop;
 
-						if (currentToast.timeout !== null) {
-							window.clearTimeout(currentToast.timeout);
-						}
-						currentToast.pausedAt = performance.now();
-						return new Map(currentMap);
-					});
-				}),
-				addMeltEventListener(node, 'pointerleave', (e) => {
-					if (isTouch(e)) return;
-					toastsMap.update((currentMap) => {
-						const currentToast = currentMap.get(node.id);
-						if (!currentToast || currentToast.closeDelay === 0) return currentMap;
+      destroy = executeCallbacks(
+        addMeltEventListener(node, 'pointerenter', (e) => {
+          if (isTouch(e)) return;
+          toastsMap.update((currentMap) => {
+            if (pauseAllOnHover.get()) {
+              for (const [, currentToast] of currentMap) {
+                pauseToastTimer(currentToast);
+              }
+            } else {
+              const currentToast = currentMap.get(node.id);
+              if (!currentToast || currentToast.closeDelay === 0)
+                return currentMap;
+              pauseToastTimer(currentToast);
+            }
+            return new Map(currentMap);
+          });
+        }),
+        addMeltEventListener(node, 'pointerleave', (e) => {
+          if (isTouch(e)) return;
+          toastsMap.update((currentMap) => {
+            if (pauseAllOnHover.get()) {
+              for (const [, currentToast] of currentMap) {
+                if (currentToast.closeDelay === 0) continue;
+                restartToastTimer(currentToast);
+              }
+            } else {
+              const currentToast = currentMap.get(node.id);
+              if (!currentToast || currentToast.closeDelay === 0)
+                return currentMap;
+              restartToastTimer(currentToast);
+            }
 
-						const pausedAt = currentToast.pausedAt ?? currentToast.createdAt;
-						const elapsed = pausedAt - currentToast.createdAt - currentToast.pauseDuration;
-						const remaining = currentToast.closeDelay - elapsed;
-						currentToast.timeout = window.setTimeout(() => {
-							removeToast(node.id);
-						}, remaining);
+            return new Map(currentMap);
+          });
+        }),
+        () => {
+          removeToast(node.id);
+        }
+      );
 
-						currentToast.pauseDuration += performance.now() - pausedAt;
-						currentToast.pausedAt = undefined;
-						return new Map(currentMap);
-					});
-				}),
-				() => {
-					removeToast(node.id);
-				}
-			);
+      return {
+        destroy
+      };
+    }
+  });
 
-			return {
-				destroy,
-			};
-		},
-	});
+  const title = makeElement(name('title'), {
+    stores: toastsMap,
+    returned: ($toasts) => {
+      return (id: string) => {
+        const toast = $toasts.get(id);
+        if (!toast) return null;
+        return {
+          id: toast.ids.title
+        };
+      };
+    }
+  });
 
-	const title = makeElement(name('title'), {
-		stores: toastsMap,
-		returned: ($toasts) => {
-			return (id: string) => {
-				const toast = $toasts.get(id);
-				if (!toast) return null;
-				return {
-					id: toast.ids.title,
-				};
-			};
-		},
-	});
+  const description = makeElement(name('description'), {
+    stores: toastsMap,
+    returned: ($toasts) => {
+      return (id: string) => {
+        const toast = $toasts.get(id);
+        if (!toast) return null;
 
-	const description = makeElement(name('description'), {
-		stores: toastsMap,
-		returned: ($toasts) => {
-			return (id: string) => {
-				const toast = $toasts.get(id);
-				if (!toast) return null;
+        return {
+          id: toast.ids.description
+        };
+      };
+    }
+  });
 
-				return {
-					id: toast.ids.description,
-				};
-			};
-		},
-	});
+  const close = makeElement(name('close'), {
+    returned: () => {
+      return (id: string) => ({
+        type: 'button' as const,
+        'data-id': id
+      });
+    },
+    action: (node: HTMLElement): MeltActionReturn<ToastEvents['close']> => {
+      function handleClose() {
+        if (!node.dataset.id) return;
+        removeToast(node.dataset.id);
+      }
 
-	const close = makeElement(name('close'), {
-		returned: () => {
-			return (id: string) => ({
-				type: 'button' as const,
-				'data-id': id,
-			});
-		},
-		action: (node: HTMLElement): MeltActionReturn<ToastEvents['close']> => {
-			function handleClose() {
-				if (!node.dataset.id) return;
-				removeToast(node.dataset.id);
-			}
+      const unsub = executeCallbacks(
+        addMeltEventListener(node, 'click', () => {
+          handleClose();
+        }),
+        addMeltEventListener(node, 'keydown', (e) => {
+          if (e.key !== kbd.ENTER && e.key !== kbd.SPACE) return;
+          e.preventDefault();
+          handleClose();
+        })
+      );
 
-			const unsub = executeCallbacks(
-				addMeltEventListener(node, 'click', () => {
-					handleClose();
-				}),
-				addMeltEventListener(node, 'keydown', (e) => {
-					if (e.key !== kbd.ENTER && e.key !== kbd.SPACE) return;
-					e.preventDefault();
-					handleClose();
-				})
-			);
+      return {
+        destroy: unsub
+      };
+    }
+  });
 
-			return {
-				destroy: unsub,
-			};
-		},
-	});
+  const toasts = derived(toastsMap, ($toastsMap) => {
+    return Array.from($toastsMap.values());
+  });
 
-	const toasts = derived(toastsMap, ($toastsMap) => {
-		return Array.from($toastsMap.values());
-	});
-
-	return {
-		elements: {
-			content,
-			title,
-			description,
-			close,
-		},
-		states: {
-			toasts: readonly(toasts),
-		},
-		helpers: {
-			addToast,
-			removeToast,
-			updateToast,
-		},
-		actions: {
-			portal: usePortal,
-		},
-		options,
-	};
+  return {
+    elements: {
+      content,
+      title,
+      description,
+      close
+    },
+    states: {
+      toasts: readonly(toasts)
+    },
+    helpers: {
+      addToast,
+      removeToast,
+      updateToast
+    },
+    actions: {
+      portal: usePortal
+    },
+    options
+  };
 }
--- src/lib/builders/toast/types.ts
+++ src/lib/builders/toast/types.ts
@@ -4,35 +4,44 @@ export type { ToastComponentEvents } from './events.js';
 export type EmptyType = Record<never, never>;
 
 export type CreateToasterProps = {
-	// Time in milliseconds before the toast is automatically closed.
-	// If set to 0, the toast will not be automatically closed.
-	closeDelay?: number;
-	type?: 'foreground' | 'background';
+  // Time in milliseconds before the toast is automatically closed.
+  // If set to 0, the toast will not be automatically closed.
+  closeDelay?: number;
+  type?: 'foreground' | 'background';
+  pauseAllOnHover?: boolean;
 };
 
 export type AddToastProps<T = object> = CreateToasterProps & {
-	data: T;
+  data: T;
 };
 
 export type Toast<T = object> = {
-	id: string;
-	ids: {
-		content: string;
-		title: string;
-		description: string;
-	};
-	closeDelay: number;
-	type: 'foreground' | 'background';
-	data: T;
-	timeout: number | null;
-	createdAt: number;
-	pausedAt?: number;
-	pauseDuration: number;
-	getPercentage: () => number;
+  id: string;
+  ids: {
+    content: string;
+    title: string;
+    description: string;
+  };
+  closeDelay: number;
+  type: 'foreground' | 'background';
+  data: T;
+  timeout: number | null;
+  createdAt: number;
+  pausedAt?: number;
+  pauseDuration: number;
+  getPercentage: () => number;
 };
 
 export type Toasts<T = object> = BuilderReturn<typeof createToaster<T>>;
-export type ToastsElements<T = object> = BuilderReturn<typeof createToaster<T>>['elements'];
-export type ToastsOptions<T = object> = BuilderReturn<typeof createToaster<T>>['options'];
-export type ToastsStates<T = object> = BuilderReturn<typeof createToaster<T>>['states'];
-export type ToastsHelpers<T = object> = BuilderReturn<typeof createToaster<T>>['helpers'];
+export type ToastsElements<T = object> = BuilderReturn<
+  typeof createToaster<T>
+>['elements'];
+export type ToastsOptions<T = object> = BuilderReturn<
+  typeof createToaster<T>
+>['options'];
+export type ToastsStates<T = object> = BuilderReturn<
+  typeof createToaster<T>
+>['states'];
+export type ToastsHelpers<T = object> = BuilderReturn<
+  typeof createToaster<T>
+>['helpers'];
