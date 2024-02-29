6097da28c10fd5a5d5f5e90f6cef4dae55486e5e
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-02-29 14:03:01 -0600

    fix: listbox programatic opening (#1020)

---
+++ .changeset/angry-lobsters-relate.md
@@ -0,0 +1,5 @@
+---
+"@melt-ui/svelte": patch
+---
+
+Listbox: fixed bug preventing programmatic opening (Closes #1014)
--- src/tests/combobox/Combobox.spec.ts
+++ src/tests/combobox/Combobox.spec.ts
@@ -1,10 +1,12 @@
-import { act, render } from '@testing-library/svelte';
+import { act, render, waitFor } from '@testing-library/svelte';
 import { userEvent } from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 import { describe } from 'vitest';
 import { testKbd as kbd } from '../utils.js';
 import ComboboxTest from './ComboboxTest.svelte';
 import type { ComboboxOptionProps } from '$lib/index.js';
+import { sleep } from '$lib/internal/helpers/sleep.js';
+import ComboboxForceVisibleTest from './ComboboxForceVisibleTest.svelte';
 
 const options: ComboboxOptionProps[] = [
 	{
@@ -153,6 +155,20 @@ describe('Combobox', () => {
 		expect(getByTestId('menu')).toBeVisible();
 	});
 
+	test('Closes on outside click by default', async () => {
+		const user = userEvent.setup();
+		const { getByTestId } = render(ComboboxTest);
+		const input = getByTestId('input');
+
+		await user.click(input);
+		const menu = getByTestId('menu');
+		expect(menu).toBeVisible();
+
+		const outsideClick = getByTestId('outside-click');
+		await user.click(outsideClick);
+		await waitFor(() => expect(menu).not.toBeVisible());
+	});
+
 	test('Closes on escape by default', async () => {
 		const user = userEvent.setup();
 		const { getByTestId } = render(ComboboxTest);
@@ -181,11 +197,249 @@ describe('Combobox', () => {
 		expect(menu).toBeVisible();
 	});
 
+	test('Can programatically open the combobox with the `open` store', async () => {
+		//
+		const user = userEvent.setup();
+		const { getByTestId } = render(ComboboxTest);
+
+		const toggleBtn = getByTestId('toggle-btn');
+		const menu = getByTestId('menu');
+
+		expect(menu).not.toBeVisible();
+		await user.click(toggleBtn);
+		await sleep(100);
+		expect(menu).toBeVisible();
+	});
+
 	test.todo('Selects multiple items when `multiple` is true');
 	test.todo('Manually setting the value updates the label');
 	test.todo('Updating options and setting the value updates the label');
-	test.todo('Respects the `closeOnEscape` prop');
 	test.todo('Respects the `closeOnOutsideClick` prop');
+	test.todo('Disabled combobox cannot be opened');
+	test.todo('Options loop when loop prop is set');
+});
+
+/**
+ *
+ * -----------------------------------------------
+ *
+ * FORCE VISIBLE
+ *
+ * -----------------------------------------------
+ */
+
+describe('Combobox (forceVisible)', () => {
+	test('No accessibility violations', async () => {
+		const { container } = render(ComboboxForceVisibleTest);
+		expect(await axe(container)).toHaveNoViolations();
+	});
+
+	test('Opens/Closes when input is clicked', async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest);
+		const input = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		expect(getMenu()).toBeNull();
+
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+
+		await user.click(input);
+		expect(getMenu()).toBeNull();
+	});
+
+	test.each(OPEN_KEYS)('Opens when %s is pressed', async (key) => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest);
+		const trigger = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		expect(getMenu()).toBeNull();
+		await act(() => trigger.focus());
+		await user.keyboard(key);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+	});
+
+	test('Selects item when clicked', async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest, { options });
+		const input = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		expect(input).not.toHaveValue(options[0].label);
+
+		expect(getMenu()).toBeNull();
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+
+		const firstItem = getMenu()?.querySelector('[data-melt-combobox-option]');
+		if (!firstItem) throw new Error('No option found');
+		await user.click(firstItem);
+
+		expect(getMenu()).toBeNull();
+
+		expect(input).toHaveValue(options[0].label);
+
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+
+		const secondItem = getMenu()?.querySelectorAll('[data-melt-combobox-option]')[1];
+		if (!secondItem) throw new Error('No option found');
+		await user.click(secondItem);
+
+		expect(queryByTestId('menu')).toBeNull();
+		expect(input).toHaveValue(options[1].label);
+	});
+
+	test('Shows correct label when defaultValue is provided', async () => {
+		const { getByTestId } = render(ComboboxForceVisibleTest, { defaultValue: 'Chocolate' });
+		const input = getByTestId('input');
+
+		expect(input).toHaveValue('Chocolate');
+	});
+
+	test('Options with undefined values can be selected', async () => {
+		const options: ComboboxOptionProps[] = [
+			{ label: '1234', value: { id: 1234, station: undefined, lastTransmission: '2023-01-01' } },
+			{ label: '4321', value: { id: 4321, station: undefined, lastTransmission: '2023-01-01' } },
+			{ label: '2341', value: { id: 2341, station: undefined, lastTransmission: '2023-01-01' } },
+			{ label: '3412', value: { id: 3412, station: undefined, lastTransmission: '2023-01-01' } },
+			{ label: '5656', value: { id: 5656, station: 'Station 1', lastTransmission: '2023-01-01' } },
+		];
+
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest, { options });
+		const input = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		expect(input).not.toHaveValue(options[0].label);
+
+		expect(getMenu()).toBeNull();
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+
+		const firstItem = getMenu()?.querySelector('[data-melt-combobox-option]');
+		if (!firstItem) throw new Error('No option found');
+
+		await user.click(firstItem);
+
+		expect(getMenu()).toBeNull();
+		expect(input).toHaveValue(options[0].label);
+
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+		expect(firstItem).toHaveAttribute('data-selected');
+	});
+
+	test('Applies custom ids when provided', async () => {
+		const user = userEvent.setup();
+		const ids = {
+			label: 'id-label',
+			menu: 'id-menu',
+			trigger: 'id-trigger',
+		};
+
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest, {
+			ids,
+		});
+
+		const trigger = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+		const label = getByTestId('label');
+		expect(trigger.id).toBe(ids.trigger);
+		await user.click(trigger);
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()?.id).toBe(ids.menu);
+		expect(label.id).toBe(ids.label);
+	});
+
+	test("Doesn't close on outside click if defaultPrevented on `onOutsideClick` handler", async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest, {
+			onOutsideClick: (e) => e.preventDefault(),
+		});
+		const input = getByTestId('input');
+
+		await user.click(input);
+		const getMenu = () => queryByTestId('menu');
+		expect(getMenu()).not.toBeNull();
+		expect(getMenu()).toBeVisible();
+
+		const outsideClick = getByTestId('outside-click');
+		await user.click(outsideClick);
+		await sleep(100);
+		expect(getMenu()).not.toBeNull();
+	});
+
+	test('Closes on outside click by default', async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxTest);
+		const input = getByTestId('input');
+
+		const getMenu = () => queryByTestId('menu');
+
+		await user.click(input);
+		expect(getMenu).not.toBeNull();
+
+		const outsideClick = getByTestId('outside-click');
+		await user.click(outsideClick);
+		await waitFor(() => expect(getMenu()).toBeNull());
+		``;
+	});
+
+	test('Closes on escape by default', async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest);
+
+		const input = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+
+		await user.keyboard(kbd.ESCAPE);
+		await waitFor(() => expect(getMenu()).toBeNull());
+	});
+
+	test('Respects the `closeOnEscape` prop', async () => {
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest, {
+			closeOnEscape: false,
+		});
+
+		const input = getByTestId('input');
+		const getMenu = () => queryByTestId('menu');
+
+		await user.click(input);
+		expect(getMenu()).not.toBeNull();
+
+		await user.keyboard(kbd.ESCAPE);
+		expect(getMenu()).not.toBeNull();
+	});
+
+	test('Can programatically open the combobox with the `open` store', async () => {
+		//
+		const user = userEvent.setup();
+		const { getByTestId, queryByTestId } = render(ComboboxForceVisibleTest);
+
+		const toggleBtn = getByTestId('toggle-btn');
+		const getMenu = () => queryByTestId('menu');
+
+		expect(getMenu()).toBeNull();
+		await user.click(toggleBtn);
+		await sleep(100);
+		expect(getMenu()).not.toBeNull();
+	});
+
+	test.todo('Selects multiple items when `multiple` is true');
+	test.todo('Manually setting the value updates the label');
+	test.todo('Updating options and setting the value updates the label');
 	test.todo('Disabled combobox cannot be opened');
 	test.todo('Options loop when loop prop is set');
 });
