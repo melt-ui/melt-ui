ef39b8ee25186daba4ab9a8de9788e8d69e28635
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-03-03 23:43:35 -0500

    fix: ScrollbarX issues (#1046)

--- src/docs/previews/scroll-area/main/tailwind/index.svelte
+++ src/docs/previews/scroll-area/main/tailwind/index.svelte
@@ -3,7 +3,16 @@
 	import { flavors } from './flavors.js';
 
 	const {
-		elements: { root, content, viewport, corner, scrollbarY, thumbY },
+		elements: {
+			root,
+			content,
+			viewport,
+			corner,
+			scrollbarY,
+			thumbY,
+			thumbX,
+			scrollbarX,
+		},
 	} = createScrollArea({
 		type: 'hover',
 		dir: 'ltr',
@@ -12,7 +21,7 @@
 
 <div
 	use:melt={$root}
-	class="relative h-72 w-56 overflow-hidden rounded-md border bg-white text-magnum-900 shadow-lg"
+	class="relative h-72 w-52 overflow-hidden rounded-md border bg-white text-magnum-900 shadow-lg"
 >
 	<div use:melt={$viewport} class="h-full w-full rounded-[inherit]">
 		<div use:melt={$content}>
@@ -35,6 +44,12 @@
 			use:melt={$thumbY}
 			class="relative flex-1 rounded-full bg-magnum-600"
 		/>
+	</div>
+	<div
+		use:melt={$scrollbarX}
+		class="flex h-2.5 w-full touch-none select-none border-t border-t-transparent bg-magnum-800/10 p-px"
+	>
+		<div use:melt={$thumbX} class="relative rounded-full bg-magnum-600" />
 	</div>
 	<div use:melt={$corner} />
 </div>
