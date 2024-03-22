51a1c736d008c8004a58bc77a10e2716891f4713
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-03-22 17:27:55 +0200

    initial tests

--- src/tests/dialog/Dialog.spec.ts
+++ src/tests/dialog/Dialog.spec.ts
@@ -307,4 +307,73 @@ describe('Dialog', () => {
 		await user.tab({ shift: true });
 		expect(getByTestId('floating-closer')).not.toHaveFocus();
 	});
+
+	it("Doesn't close when interacting outside with click interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('click-interceptor'));
+		expect(content).toBeVisible();
+	});
+
+	it("Doesn't close when interacting outside with pointerdown interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('pointerdown-interceptor'));
+		expect(content).toBeVisible();
+	});
+	it("Doesn't close when interacting outside with pointerup interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('pointerup-interceptor'));
+		expect(content).toBeVisible();
+	});
+
+	it("Doesn't close when interacting outside with mousedown interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('mousedown-interceptor'));
+		expect(content).toBeVisible();
+	});
+
+	it("Doesn't close when interacting outside with mouseup interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('mouseup-interceptor'));
+		expect(content).toBeVisible();
+	});
+
+	it("Doesn't close when interacting outside with touchstart interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('touchstart-interceptor'));
+		expect(content).toBeVisible();
+	});
+
+	it("Doesn't close when interacting outside with touchend interceptor", async () => {
+		const { getByTestId, user, trigger } = setup();
+		const content = getByTestId('content');
+		expect(content).not.toBeVisible();
+		await user.click(trigger);
+		expect(content).toBeVisible();
+		await user.click(getByTestId('touchend-interceptor'));
+		expect(content).toBeVisible();
+	});
 });
