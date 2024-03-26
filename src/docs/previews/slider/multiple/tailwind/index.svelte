0b4df4849e7649153c29d91cf687132f9ffdaa9c
bennymi <xbenmmusicx@gmail.com>
2024-03-26 20:26:39 +0100

    update example

--- src/docs/previews/slider/multiple/tailwind/index.svelte
+++ src/docs/previews/slider/multiple/tailwind/index.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
 	import Slider from './slider.svelte';
+
+	const value1 = writable([30, 40, 50, 60, 70]);
+	const value2 = writable([30, 40, 50, 60, 70]);
 </script>
 
 <div class="flex flex-col gap-8">
 	<div class="flex flex-col gap-4 text-center">
-		<code>enableSwap: false</code>
-		<Slider />
+		<code>enableSwap: false; value: {$value1}</code>
+		<Slider value={value1} />
 	</div>
 	<div class="flex flex-col gap-4 text-center">
-		<code>enableSwap: true</code>
-		<Slider enableSwap={true} />
+		<code>enableSwap: true; value: {$value2}</code>
+		<Slider value={value2} enableSwap={true} />
 	</div>
 </div>
--- src/docs/previews/slider/multiple/tailwind/slider.svelte
+++ src/docs/previews/slider/multiple/tailwind/slider.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { createSlider, melt } from '$lib/index.js';
+	import type { Writable } from 'svelte/store';
 
+	export let value: Writable<number[]>;
 	export let enableSwap = false;
 
 	const {
 		elements: { root, range, thumbs },
 	} = createSlider({
-		defaultValue: [20, 30, 40, 80],
+		value,
 		max: 100,
 		enableSwap,
 	});
 </script>
 
-<span use:melt={$root} class="relative flex h-[20px] w-[200px] items-center">
+<span use:melt={$root} class="relative flex h-[20px] items-center">
 	<span class="h-[3px] w-full bg-white">
 		<span use:melt={$range} class="h-[3px] bg-white" />
 	</span>
