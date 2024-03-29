61cdbd3f3a0a481b4914c7827e31cea94578d305
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-03-29 17:38:22 +0300

    Fix(focus trap): trap deactivation intercepted interact outside (#1135)

--- src/lib/internal/actions/focus-trap/action.ts
+++ src/lib/internal/actions/focus-trap/action.ts
@@ -35,6 +35,7 @@ export function createFocusTrap(config: FocusTrapConfig = {}) {
 
 	const useFocusTrap = (node: HTMLElement) => {
 		trap = _createFocusTrap(node, {
+			clickOutsideDeactivates: false,
 			...focusTrapOptions,
 			onActivate() {
 				hasFocus.set(true);
