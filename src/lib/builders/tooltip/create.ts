1bde2ad48acceb7115d40bb45269bdd0a6d5641c
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-04-07 18:09:06 +0300

    chore: refactor isPointerInGraceArea (#1159)

--- src/lib/builders/tooltip/create.ts
+++ src/lib/builders/tooltip/create.ts
@@ -15,10 +15,10 @@ import {
 	noop,
 	omit,
 	overridable,
-	pointInPolygon,
 	styleToString,
 	toWritableStores,
 	portalAttr,
+	isPointerInGraceArea,
 } from '$lib/internal/helpers/index.js';
 
 import { useFloating, usePortal } from '$lib/internal/actions/index.js';
@@ -306,14 +306,7 @@ export function createTooltip(props?: CreateTooltipProps) {
 					? [triggerEl]
 					: [triggerEl, contentEl];
 				const polygon = makeHullFromElements(polygonElements);
-
-				isMouseInTooltipArea = pointInPolygon(
-					{
-						x: e.clientX,
-						y: e.clientY,
-					},
-					polygon
-				);
+				isMouseInTooltipArea = isPointerInGraceArea(e, polygon);
 
 				if ($openReason !== 'pointer') return;
 
