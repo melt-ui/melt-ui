d54e71132430a1869f647ad1e9967b74086e595c
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-06-28 15:20:11 +0100

    lint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,7 +29,6 @@
 	import { ModeWatcher } from 'mode-watcher';
 
 	$: isRoot = $page.url.pathname === '/';
-	let skipToContent = false;
 </script>
 
 <svelte:head>
@@ -50,7 +49,7 @@
 	href="#main"
 	on:blur={() => (skipToContent = false)}
 	on:focus={() => (skipToContent = true)}
-	class="force-dark focus:z-[1000] fixed top-[-20rem] z-0 ml-56 rounded-br-xl rounded-tl-xl bg-neutral-900 px-2 py-1 text-neutral-100 focus:top-4 max-sm:right-[6]"
+	class="force-dark fixed top-[-20rem] z-0 ml-56 rounded-br-xl rounded-tl-xl bg-neutral-900 px-2 py-1 text-neutral-100 focus:top-4 focus:z-[1000] max-sm:right-[6]"
 	tabindex="0"
 >
 	Skip to main content
