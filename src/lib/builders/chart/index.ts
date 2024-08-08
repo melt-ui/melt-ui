951a2427882fd4871d724f95a0d979dcd0ba50c9
Will Henderson <git@mail.jjbig.com>
2024-08-08 12:21:26 +1000

    adding utility type guards

--- src/lib/builders/chart/index.ts
+++ src/lib/builders/chart/index.ts
@@ -1,4 +1,6 @@
-export { createChart  } from './create.js';
-export { h_range, h_linear, h_band, v_range, v_linear, v_band } from './cardinal.js';
 export type { ChartBasics, Dimension, DimensionDiscrete, DimensionContinuous } from './types-create.js'
 export type { ChartBasics_Describe, Dimension_Describe, DimensionDiscrete_Describe, DimensionContinuous_Describe } from './types-describe.js'
+
+export { createChart  } from './create.js';
+export { h_range, h_linear, h_band, v_range, v_linear, v_band } from './cardinal.js';
+export { is_discrete, is_continuous } from './util.js';
--- src/lib/builders/chart/util.ts
+++ src/lib/builders/chart/util.ts
@@ -1,4 +1,6 @@
 import { type Readable, writable } from 'svelte/store';
+import type { Dimension, DimensionContinuous, DimensionDiscrete } from './types-create.js';
+import type { Scale } from './types-basic.js';
 
 export function isStore<TYPE>(maybeStore: TYPE | Readable<TYPE>): maybeStore is Readable<TYPE> {
 	return maybeStore && typeof maybeStore === 'object' && 'subscribe' in maybeStore;
@@ -14,5 +16,17 @@ export function makeStore<TYPE>(maybeStore: TYPE | Readable<TYPE>): Readable<TYP
 
 export function tuple<T extends any[]>(...args: T): T {
 	return args;
+}
+
+export function is_discrete<ROW, META, DOMAINTYPE extends DOMAINSIMPLETYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER extends Scale<DOMAINSIMPLETYPE, RANGETYPE>>(
+	dimension: Dimension<ROW, META, DOMAINTYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER>
+) : dimension is DimensionDiscrete<ROW, META, DOMAINTYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER> {
+	return dimension.discrete;
+}
+
+export function is_continuous<ROW, META, DOMAINTYPE extends DOMAINSIMPLETYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER extends Scale<DOMAINSIMPLETYPE, RANGETYPE>>(
+	dimension: Dimension<ROW, META, DOMAINTYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER>
+) : dimension is DimensionContinuous<ROW, META, DOMAINTYPE, RANGETYPE, DOMAINSIMPLETYPE, SCALER> {
+	return !dimension.discrete;
 }
 
